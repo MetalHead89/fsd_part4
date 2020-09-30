6569c8814b8c35c1571db2adb0a9ba48fa5e7d41
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2020-09-30 16:13:09 +0700

    Created mvc base, created Slider and Thumb classes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import '@/pug/modules/slider/slider.ts'
+
 import '@/style.scss';
 import '@/pug/modules/slider/slider.scss'
--- src/pug/modules/slider/slider.ts
+++ src/pug/modules/slider/slider.ts
@@ -1,11 +1,86 @@
+////          Classes          ////
+
 class Slider {
     slider: HTMLElement;
     track: HTMLElement | null;
-    thumb: HTMLElement | null;
+    thumb: Thumb;
 
     constructor(slider: HTMLElement) {
         this.slider = slider;
         this.track = slider.querySelector('.slider__track');
-        this.thumb = slider.querySelector('.slider__thumb');
+        this.thumb = new Thumb(slider.querySelector('.slider__thumb'));
     }
 }
+
+class Thumb {
+    color: string = '';
+    thumb: HTMLElement | null;
+
+    constructor(thumb: HTMLElement | null) {
+        this.thumb = thumb;
+
+        if (this.thumb)
+            this.thumb.onclick = () => {alert('111')};
+    }
+}
+
+
+
+
+////          Interfaces          ////
+
+interface Presenter {
+    initialize(): void;
+}
+
+interface View {
+    getSliders(): NodeListOf<HTMLElement>;
+};
+
+interface Model {
+    createSlider(slider: HTMLElement) :void;
+}
+
+
+
+
+////          Presenter          ////
+
+const presenter: Presenter = {
+    initialize() {
+        const sliders: NodeListOf<HTMLElement> = view.getSliders();
+
+        for (let slider of sliders) {
+            model.createSlider(slider)
+        }
+    }
+};
+
+
+
+
+////          View          ////
+
+const view: View = {
+    getSliders() {
+        return document.querySelectorAll('.slider')
+    }
+};
+
+
+
+
+////          Model          ////
+
+const model: Model = {
+    createSlider(sliderHTMLElement) {
+        const slider: Slider = new Slider(sliderHTMLElement);
+    }
+}
+
+
+
+
+////          Start application          ////
+
+presenter.initialize();
