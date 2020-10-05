793b9235facf00db9862c5fbb809256fa894a210
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2020-10-05 16:29:40 +0700

    the view, model and presenter objects are converted into classes

--- src/pug/modules/slider/slider.ts
+++ src/pug/modules/slider/slider.ts
@@ -1,12 +1,10 @@
-////          Classes          ////
-
 class Slider {
-    slider: HTMLElement;
+    element: HTMLElement;
     track: Track;
     thumb: Thumb;
 
     constructor(slider: HTMLElement) {
-        this.slider = slider;
+        this.element = slider;
         this.track = new Track(slider.querySelector('.slider__track'));
         this.thumb = new Thumb(slider.querySelector('.slider__thumb'));
     }
@@ -14,104 +12,222 @@ class Slider {
 
 class Thumb {
     color: string = '';
-    thumb: HTMLElement | null;
+    element: HTMLElement | null;
 
     constructor(thumb: HTMLElement | null) {
-        this.thumb = thumb;
+        this.element = thumb;
 
-        if (this.thumb) {
-            this.thumb.addEventListener('mousedown', presenter.onMouseDownThumb);
-            // this.thumb.onmousedown = function(event) {
-            //     moveAt(this.thumb, event.pageX, event.pageY);
+        // if (this.element) {
+        //     this.element.addEventListener('mousedown', presenter.onMouseDownThumb);
+        //     // this.thumb.onmousedown = function(event) {
+        //     //     moveAt(this.thumb, event.pageX, event.pageY);
 
-            //     function moveAt(thumb:HTMLElement, pageX: number, pageY:number) {
-            //         this.style.left = pageX - this.offsetWidth / 2 + 'px';
-            //         ball.style.top = pageY - ball.offsetHeight / 2 + 'px';
-            //     }
-            // }
-        }            
+        //     //     function moveAt(thumb:HTMLElement, pageX: number, pageY:number) {
+        //     //         this.style.left = pageX - this.offsetWidth / 2 + 'px';
+        //     //         ball.style.top = pageY - ball.offsetHeight / 2 + 'px';
+        //     //     }
+        //     // }
+        // }            
     }
 }
 
 class Track {
     color: string = '';
-    track: HTMLElement | null;
+    element: HTMLElement | null;
 
     constructor(track: HTMLElement | null) {
-        this.track = track;
+        this.element = track;
     }
 }
 
-class Scale {
+class View {
 
-}
+    constructor() {
+        const sliders: NodeListOf<HTMLElement> = document.querySelectorAll('.slider');
 
-
-
-
-////          Interfaces          ////
-
-interface Presenter {
-    initialize(): void;
-    onMouseDownThumb(): void;
-}
-
-interface View {
-    getSliders(): NodeListOf<HTMLElement>;
-    moveThumb(thumb: Thumb): void;
-};
-
-interface Model {
-    createSlider(slider: HTMLElement) :void;
-}
-
-
-
-
-////          Presenter          ////
-
-const presenter: Presenter = {
-    initialize() {
-        const sliders: NodeListOf<HTMLElement> = view.getSliders();
-
-        for (let slider of sliders) {
-            model.createSlider(slider)
+        for (let sliderElement of sliders) {
+            const slider: Slider = new Slider(sliderElement);
+            // if (slider.thumb.element) {
+            //     slider.thumb.element.addEventListener('mousedown', () => {
+            //         this.style.left = pageX - this.offsetWidth / 2 + 'px';
+            //         slider.thumb.element.style.top = pageY - ball.offsetHeight / 2 + 'px';
+            //     })
+            // }
         }
-    },
-    onMouseDownThumb() {
-        console.log(this);
     }
+
+    moveThumb(thumb: HTMLElement): void {
+
+    }
+    // getSliders() {
+    //     return document.querySelectorAll('.slider')
+    // },
+    // moveThumb(thumb) {
+    //     console.log(thumb)
+    // }
+}
+
+class Model {
+
+    constructor() {
+
+    }
+    // createSlider(sliderHTMLElement) {
+    //     const slider: Slider = new Slider(sliderHTMLElement);
+    // }
+}
+
+class Presenter {
+    view: View;
+    model: Model;
+
+    constructor(view: View, model: Model) {
+        this.view = view;
+        this.model = model;
+    }
+
+    // initialize() {
+    //     const sliders: NodeListOf<HTMLElement> = view.getSliders();
+
+    //     for (let slider of sliders) {
+    //         model.createSlider(slider)
+    //     }
+    // }
+
+    // onMouseDownThumb() {
+    //     console.log(this);
+    // }
     
-};
+}
 
-
-
-
-////          View          ////
-
-const view: View = {
-    getSliders() {
-        return document.querySelectorAll('.slider')
-    },
-    moveThumb(thumb) {
-        console.log(thumb)
-    }
-};
-
-
-
-
-////          Model          ////
-
-const model: Model = {
-    createSlider(sliderHTMLElement) {
-        const slider: Slider = new Slider(sliderHTMLElement);
-    }
+window.onload = () => {
+    const model: Model = new Model();
+    const view: View = new View();
+    const presenter: Presenter = new Presenter(view, model);
 }
 
 
 
 
-////          Start application          ////
 
-presenter.initialize();
+
+
+
+
+// ////          Classes          ////
+
+// class Slider {
+//     slider: HTMLElement;
+//     track: Track;
+//     thumb: Thumb;
+
+//     constructor(slider: HTMLElement) {
+//         this.slider = slider;
+//         this.track = new Track(slider.querySelector('.slider__track'));
+//         this.thumb = new Thumb(slider.querySelector('.slider__thumb'));
+//     }
+// }
+
+// class Thumb {
+//     color: string = '';
+//     thumb: HTMLElement | null;
+
+//     constructor(thumb: HTMLElement | null) {
+//         this.thumb = thumb;
+
+//         if (this.thumb) {
+//             this.thumb.addEventListener('mousedown', presenter.onMouseDownThumb);
+//             // this.thumb.onmousedown = function(event) {
+//             //     moveAt(this.thumb, event.pageX, event.pageY);
+
+//             //     function moveAt(thumb:HTMLElement, pageX: number, pageY:number) {
+//             //         this.style.left = pageX - this.offsetWidth / 2 + 'px';
+//             //         ball.style.top = pageY - ball.offsetHeight / 2 + 'px';
+//             //     }
+//             // }
+//         }            
+//     }
+// }
+
+// class Track {
+//     color: string = '';
+//     track: HTMLElement | null;
+
+//     constructor(track: HTMLElement | null) {
+//         this.track = track;
+//     }
+// }
+
+// class Scale {
+
+// }
+
+
+
+
+// ////          Interfaces          ////
+
+// interface Presenter {
+//     initialize(): void;
+//     onMouseDownThumb(): void;
+// }
+
+// interface View {
+//     getSliders(): NodeListOf<HTMLElement>;
+//     moveThumb(thumb: Thumb): void;
+// };
+
+// interface Model {
+//     createSlider(slider: HTMLElement) :void;
+// }
+
+
+
+
+// ////          Presenter          ////
+
+// const presenter: Presenter = {
+//     initialize() {
+//         const sliders: NodeListOf<HTMLElement> = view.getSliders();
+
+//         for (let slider of sliders) {
+//             model.createSlider(slider)
+//         }
+//     },
+//     onMouseDownThumb() {
+//         console.log(this);
+//     }
+    
+// };
+
+
+
+
+// ////          View          ////
+
+// const view: View = {
+//     getSliders() {
+//         return document.querySelectorAll('.slider')
+//     },
+//     moveThumb(thumb) {
+//         console.log(thumb)
+//     }
+// };
+
+
+
+
+// ////          Model          ////
+
+// const model: Model = {
+//     createSlider(sliderHTMLElement) {
+//         const slider: Slider = new Slider(sliderHTMLElement);
+//     }
+// }
+
+
+
+
+// ////          Start application          ////
+
+// presenter.initialize();
