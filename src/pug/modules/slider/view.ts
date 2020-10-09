4ffeb157cb38a10d876dd98e6f3b31ed7e780ab2
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2020-10-09 16:07:22 +0700

    added movement thumb code to view class

--- src/pug/modules/slider/view.ts
+++ src/pug/modules/slider/view.ts
@@ -32,9 +32,55 @@ export class View {
         document.body.append(slider);
         sliderPosition.replaceWith(slider);
 
+        slider.addEventListener('mousedown', event => { this.sliderOnMouseDown(slider, event) });
+
         this.observer.notify('addedNewSlider', 
             {'slider': slider, 'track': track,  'thumb': thumb});
     }
+
+    sliderOnMouseDown(slider: HTMLElement, event: MouseEvent): void {
+        const target: HTMLElement = event.target as HTMLElement;
+
+        if (target.classList.contains('slider__thumb')) {
+            this.moveThumb(slider, target, event);
+        }
+    }
+
+    moveThumb(slider: HTMLElement, thumb: HTMLElement, event: MouseEvent): void {
+
+        let coords: { [index: string]: number } = this.getCoords(thumb);
+        let shiftX: number = event.pageX - coords.left;
+
+        moveAt(event);
+        
+        document.addEventListener('mousemove', moveAt);
+        document.addEventListener('mouseup', documentOnMouseUp);
+        thumb.ondragstart = function () { return false; };
+
+        function moveAt(event: MouseEvent) {
+            thumb.style.left = event.pageX - slider.offsetLeft - shiftX + 'px';
+
+            if (parseInt(thumb.style.left) < 0) {
+                thumb.style.left = '0px';
+            } else if (parseInt(thumb.style.left) > slider.offsetWidth - thumb.offsetWidth) {
+                thumb.style.left = slider.offsetWidth - thumb.offsetWidth + 'px';
+            }
+        }
+
+        function documentOnMouseUp() {
+            document.removeEventListener('mousemove', moveAt)
+            document.removeEventListener('mouseup', documentOnMouseUp)
+        }
+    }
+
+    getCoords(elem: HTMLElement): { [index: string]: number } {
+        let box: DOMRect = elem.getBoundingClientRect();
+        return {
+            top: box.top + pageYOffset,
+            left: box.left + pageXOffset
+        };
+    }
+
 }
 
 // export class View {
