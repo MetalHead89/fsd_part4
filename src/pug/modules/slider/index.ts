1652250bf4103abdf7268ba564cedca08b4cdcb0
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-10-17 21:58:45 +0700

    Added jQuery plugin wrapper

--- src/pug/modules/slider/index.ts
+++ src/pug/modules/slider/index.ts
@@ -1,11 +1,15 @@
-import {Observable} from '../slider/observable';
-import {Model} from '../slider/model';
-import {View} from '../slider/view';
-import {Presenter} from '../slider/presenter';
+import { Observable } from '../slider/observable';
+import { Model } from '../slider/model';
+import { View } from '../slider/view';
+import { Presenter } from '../slider/presenter';
 
-const observer = new Observable();
-const model: Model = new Model(observer);
-const view: View = new View(observer);
-const presenter: Presenter = new Presenter(view, model, observer);
+(function ($) {
+    ($.fn as any).incredibleSliderPlugin = function () {
+        const observer = new Observable();
+        const model: Model = new Model(observer);
+        const view: View = new View(observer);
+        const presenter: Presenter = new Presenter(view, model, observer);
+    };
+})(jQuery);
 
-presenter.init();
+// presenter.init();
