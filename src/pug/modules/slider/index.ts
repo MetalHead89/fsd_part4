60d5fe3e4135ffac89d1afd2d0fb5f2cdfa2da31
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-10-18 19:59:28 +0700

    Added comments to plugin file

--- src/pug/modules/slider/index.ts
+++ src/pug/modules/slider/index.ts
@@ -5,24 +5,29 @@ import { Presenter } from '../slider/presenter';
 import { iPluginSettings } from '../slider/interfaces';
 
 (function ($) {
-
     ($.fn as any).incredibleSliderPlugin = function (options: iPluginSettings) {
+
         const observer = new Observable();
         const model: Model = new Model(observer);
         const view: View = new View(observer);
         const presenter: Presenter = new Presenter(view, model, observer);
 
+        // Настройки плагина        
         let settings: iPluginSettings = {
             'orienation': 'horizontal',
             'type': 'single'
         }
 
+        // Обновление настроек плагина в соответсвии с полученными параметрами
         settings = $.extend(settings, options);
 
-        for (const elem of this) {
-            presenter.init(elem);
+        // Инициализация слайдеров
+        for (const sliderElem of this) {
+            presenter.init(sliderElem);
         }
+
     };
 })(jQuery);
 
+// Поиск блоков с классом incredibleSliderPlugin и передача их плагину для добавления в них слайдеров
 ($('.incredibleSliderPlugin') as any).incredibleSliderPlugin();
