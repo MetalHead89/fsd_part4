887e97e28677531fc34f102e7f4ba2c0dbcc2539
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-10-21 21:20:36 +0700

    Added min, max and step inputs to sliderPanelPlugin

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import '@/plugins/sliderPlugin/index.ts'
 import '@/plugins/sliderPanelPlugin/index.ts'
 
 import '@/style.scss';
-import '@/plugins/sliderPlugin/styles/slider.scss'
+import '@/plugins/sliderPlugin/styles/style.scss'
+import '@/plugins/sliderPanelPlugin/styles/style.scss'
--- src/plugins/sliderPanelPlugin/panel.ts
+++ src/plugins/sliderPanelPlugin/panel.ts
@@ -9,19 +9,29 @@ export class Panel {
         this.sliderPanel.className = 'slider-panel';
 
         this.minValue = document.createElement('input');
-        this.minValue.className = 'slider-panel';
-
         this.maxValue = document.createElement('input');
-        this.maxValue.className = 'slider-panel';
-
         this.step = document.createElement('input');
-        this.step.className = 'slider-panel';
 
-        this.sliderPanel.append(this.minValue);
-        this.sliderPanel.append(this.maxValue);
-        this.sliderPanel.append(this.step);
-
+        this.sliderPanel.append(this.createInputControl(this.minValue, 'slider-panel__input', 'min'));
+        this.sliderPanel.append(this.createInputControl(this.maxValue, 'slider-panel__input', 'max'));
+        this.sliderPanel.append(this.createInputControl(this.step, 'slider-panel__input', 'step'));
         
         panelPosition.replaceWith(this.sliderPanel);
+    }
+
+    private createInputControl(control:HTMLElement, controlClass: string, labelText: string): HTMLElement {
+        control.setAttribute('type', 'text');
+        control.className = controlClass;
+
+        const controlLabel: HTMLElement = document.createElement('label');
+        controlLabel.textContent = labelText;
+
+        const wrapper: HTMLElement = document.createElement('div');
+        wrapper.className = 'slider-panel__control-wrapper';
+
+        wrapper.append(controlLabel)
+        wrapper.append(control);
+
+        return wrapper;
     }
 }
