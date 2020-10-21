7189c01a46bc70247e993e13dc3d4d9f73ad6b1e
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2020-10-21 16:45:25 +0700

    Added 3 text boxes to sliderPanelPlugin

--- src/plugins/sliderPanelPlugin/panel.ts
+++ src/plugins/sliderPanelPlugin/panel.ts
@@ -1,8 +1,27 @@
 export class Panel {
+    sliderPanel: HTMLElement | null = null;
+    minValue: HTMLElement | null = null;
+    maxValue: HTMLElement | null = null;
+    step: HTMLElement | null = null;
+
     constructor(panelPosition: HTMLElement) {
-        const sliderPanel: HTMLElement = document.createElement('div');
-        sliderPanel.className = 'slider-panel';
+        this.sliderPanel = document.createElement('div');
+        this.sliderPanel.className = 'slider-panel';
+
+        this.minValue = document.createElement('input');
+        this.minValue.className = 'slider-panel';
+
+        this.maxValue = document.createElement('input');
+        this.maxValue.className = 'slider-panel';
+
+        this.step = document.createElement('input');
+        this.step.className = 'slider-panel';
+
+        this.sliderPanel.append(this.minValue);
+        this.sliderPanel.append(this.maxValue);
+        this.sliderPanel.append(this.step);
+
         
-        panelPosition.replaceWith(sliderPanel);
+        panelPosition.replaceWith(this.sliderPanel);
     }
 }
