fc2cc3630a92e368a4f9f798b1f98f2163861b04
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-10-22 23:18:40 +0700

    Changed initialization logic of the slider plugin

--- src/plugins/sliderPlugin/index.ts
+++ src/plugins/sliderPlugin/index.ts
@@ -5,30 +5,42 @@ import { Presenter } from './presenter';
 import { IPluginSettings } from './interfaces';
 
 (function ($) {
-    ($.fn as any).incredibleSliderPlugin = function (options: IPluginSettings) {
 
-        // Настройки плагина        
-        let settings: IPluginSettings = {
-            'orienation': 'horizontal',
-            'type': 'single',
-            'minValue': 0,
-            'maxValue': 100,
-            'step': 1
+    // Настройки плагина
+    const defaultSettings: any = {
+        'orienation': 'horizontal',
+        'type': 'single',
+        'minValue': 0,
+        'maxValue': 100,
+        'step': 1
+    };
+
+    const methods: any = {
+        init: function () {
+            // Обновление настроек плагина в соответсвии с полученными параметрами
+            // const settings: any = $.extend(defaultSettings, options);
+            console.log(this)
+            // Добавление конфигурации новых слайдеров в модель
+            for (const sliderPosition of this) {
+                const observer = new Observable();
+                const model: Model = new Model(observer);
+                const view: View = new View(observer);
+                const presenter: Presenter = new Presenter(view, model, observer);
+
+                // model.createNewSlider(sliderPosition, settings);
+                model.createNewSlider(sliderPosition, defaultSettings);
+            }
         }
+    };
 
-        // Обновление настроек плагина в соответсвии с полученными параметрами
-        settings = $.extend(settings, options);
-
-        // Добавление конфигурации новых слайдеров в модель
-        for (const sliderPosition of this) {
-            const observer = new Observable();
-            const model: Model = new Model(observer);
-            const view: View = new View(observer);
-            const presenter: Presenter = new Presenter(view, model, observer);
-
-            model.createNewSlider(sliderPosition, settings);
+    ($.fn as any).incredibleSliderPlugin = function (action?: string | IPluginSettings) {
+        if (typeof action === 'object' || !action) {
+            return methods.init.apply(this, arguments);
+        } else if (methods[action]) {
+            return methods[action].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else {
+            $.error('Метод с именем ' + action + ' не существует для jQuery.incredibleSliderPlugin');
         }
-
     };
 })(jQuery);
 
