a5d2fe29107f4c942932f3b8879e0d6e3ba0a463
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-10-31 16:26:09 +0700

    Old code commented out, plugin wrapper and default settings created

--- src/plugins/sliderPlugin/incredibleSliderPlugin.ts
+++ src/plugins/sliderPlugin/incredibleSliderPlugin.ts
@@ -2,74 +2,104 @@ import { Observable } from './observable';
 import { Model } from './model';
 import { View } from './view';
 import { Presenter } from './presenter';
-import { IPluginSettings } from './interfaces';
-import { IgnorePlugin } from 'webpack';
+// import { IPluginSettings } from './interfaces';
+// import { IgnorePlugin } from 'webpack';
 
 (function ($) {
 
-    // Настройки плагина
+    // Настройки плагина по умолчанию
     const defaultSettings: any = {
         'orienation': 'horizontal',
         'type': 'single',
-        'scale': true,
+        'scale': true,        
         'minValue': 1,
-        'maxValue': 100,
+        'maxValue': 10,
         'step': 1
     };
 
-    const methods: any = {
-        init: function (options: IPluginSettings) {
-            // Обновление настроек плагина в соответсвии с полученными параметрами
-            const settings: any = $.extend(defaultSettings, options);/////////////////////////////////////////////////////////////////
-
-            // Добавление конфигурации новых слайдеров в модель
-            return this.each(function (this: any) { //////////////////////////////////////////////////////////////////////////////////
-                const observer = new Observable();
-                const model: Model = new Model(observer);
-                const view: View = new View(observer);
-                const presenter: Presenter = new Presenter(view, model, observer);
-
-                model.createNewSlider(this, settings);
-
-                $(this).data('settings', settings); ///////////////////////// добавить проверку на существование объекта
-                $(this).data('model', model);
-                // $(this).data('slider', {
-                //     target: $(this),
-                //     m: model
-                // });
-            });
-        },
-        setMinValue: function(value: number) {
-            $(this).data().model.setMinValue(value);
-        },
-        setMaxValue: function(value: number) {
-            $(this).data().model.setMaxValue(value);
-        },
-        setStepValue: function(value: number) {
-            $(this).data().model.setStepValue(value);
-        }
-    };
-
-    $.fn.incredibleSliderPlugin = function (action?: string | IPluginSettings, args?): JQuery<HTMLElement> {
-        if (typeof action === 'string' && methods[action]) {
-            return methods[action].call(this, args);
-        } else if (typeof action === 'object' || !action) {
-            return methods.init.call(this, args);
-        } else {
-            $.error('Метод с именем ' + action + ' не существует для jQuery.incredibleSliderPlugin');
-            return this;
-        }
-        // if (typeof action === 'object' || !action) {
-        //     return methods.init.apply(this, arguments);
-        // } else if (methods[action]) {
-        //     return methods[action].apply(this, Array.prototype.slice.call(arguments, 1));
-        // } else {
-        //     $.error('Метод с именем ' + action + ' не существует для jQuery.incredibleSliderPlugin');
-        //     return this;
-        // }
-    };
 })(jQuery);
+
+
+
+
 
 // // Поиск блоков с классом incredibleSliderPlugin и передача их плагину для добавления в них слайдеров
 // $('.incredibleSliderPlugin').incredibleSliderPlugin();
 // console.log($('.incredibleSliderPlugin').eq(0).incredibleSliderPlugin('show'))
+
+
+// import { Observable } from './observable';
+// import { Model } from './model';
+// import { View } from './view';
+// import { Presenter } from './presenter';
+// import { IPluginSettings } from './interfaces';
+// import { IgnorePlugin } from 'webpack';
+
+// (function ($) {
+
+//     // Настройки плагина
+//     const defaultSettings: any = {
+//         'orienation': 'horizontal',
+//         'type': 'single',
+//         'scale': true,
+//         'minValue': 1,
+//         'maxValue': 100,
+//         'step': 1
+//     };
+
+//     const methods: any = {
+//         init: function (options: IPluginSettings) {
+//             // Обновление настроек плагина в соответсвии с полученными параметрами
+//             const settings: any = $.extend(defaultSettings, options);/////////////////////////////////////////////////////////////////
+
+//             // Добавление конфигурации новых слайдеров в модель
+//             return this.each(function (this: any) { //////////////////////////////////////////////////////////////////////////////////
+//                 const observer = new Observable();
+//                 const model: Model = new Model(observer);
+//                 const view: View = new View(observer);
+//                 const presenter: Presenter = new Presenter(view, model, observer);
+
+//                 model.createNewSlider(this, settings);
+
+//                 $(this).data('settings', settings); ///////////////////////// добавить проверку на существование объекта
+//                 $(this).data('model', model);
+//                 // $(this).data('slider', {
+//                 //     target: $(this),
+//                 //     m: model
+//                 // });
+//             });
+//         },
+//         setMinValue: function(value: number) {
+//             $(this).data().model.setMinValue(value);
+//         },
+//         setMaxValue: function(value: number) {
+//             $(this).data().model.setMaxValue(value);
+//         },
+//         setStepValue: function(value: number) {
+//             $(this).data().model.setStepValue(value);
+//         }
+//     };
+
+//     $.fn.incredibleSliderPlugin = function (action?: string | IPluginSettings, args?): JQuery<HTMLElement> {
+//         if (typeof action === 'string' && methods[action]) {
+//             return methods[action].call(this, args);
+//         } else if (typeof action === 'object' || !action) {
+//             return methods.init.call(this, args);
+//         } else {
+//             $.error('Метод с именем ' + action + ' не существует для jQuery.incredibleSliderPlugin');
+//             return this;
+//         }
+//         // if (typeof action === 'object' || !action) {
+//         //     return methods.init.apply(this, arguments);
+//         // } else if (methods[action]) {
+//         //     return methods[action].apply(this, Array.prototype.slice.call(arguments, 1));
+//         // } else {
+//         //     $.error('Метод с именем ' + action + ' не существует для jQuery.incredibleSliderPlugin');
+//         //     return this;
+//         // }
+//     };
+// })(jQuery);
+
+// // // Поиск блоков с классом incredibleSliderPlugin и передача их плагину для добавления в них слайдеров
+// // $('.incredibleSliderPlugin').incredibleSliderPlugin();
+// // console.log($('.incredibleSliderPlugin').eq(0).incredibleSliderPlugin('show'))
--- src/plugins/sliderPlugin/interfaces.ts
+++ src/plugins/sliderPlugin/interfaces.ts
@@ -1,44 +1,49 @@
-export interface IPluginSettings {
-    'orienation': string,
-    'type': string,
-    'scale': boolean,
-    'minValue': number,
-    'maxValue': number,
-    'step': number
-}
+// export interface IdefaultSettings {
+    
+// }
 
-export interface IGroupedSettings {
-    'sliderSettings': ISliderSettings
-    'thumbSettings': IThumbSettings,
-    'scaleSettings': IScaleSettings
-}
 
-export interface INewSliderOptions {
-    'sliderPosition': HTMLElement,
-    'settings': IGroupedSettings
-}
+// export interface IPluginSettings {
+//     'orienation': string,
+//     'type': string,
+//     'scale': boolean,
+//     'minValue': number,
+//     'maxValue': number,
+//     'step': number
+// }
 
-export interface ISliderComponents {
-    'sliderElem': HTMLElement,
-    'trackElem': HTMLElement,
-    'progressBar': HTMLElement,
-    'thumbElem': HTMLElement,
-    'scaleElem': HTMLElement
-}
+// export interface IGroupedSettings {
+//     'sliderSettings': ISliderSettings
+//     'thumbSettings': IThumbSettings,
+//     'scaleSettings': IScaleSettings
+// }
 
-export interface IThumbSettings {
-    'minValue': number,
-    'maxValue': number,
-    'step': number
-}
+// export interface INewSliderOptions {
+//     'sliderPosition': HTMLElement,
+//     'settings': IGroupedSettings
+// }
 
-export interface ISliderSettings {
-    'orientation': string,
-    'type': string,
-}
+// export interface ISliderComponents {
+//     'sliderElem': HTMLElement,
+//     'trackElem': HTMLElement,
+//     'progressBar': HTMLElement,
+//     'thumbElem': HTMLElement,
+//     'scaleElem': HTMLElement
+// }
 
-export interface IScaleSettings {
-    'displayed': boolean,
-    'minValue': number,
-    'maxValue': number,
-}
+// export interface IThumbSettings {
+//     'minValue': number,
+//     'maxValue': number,
+//     'step': number
+// }
+
+// export interface ISliderSettings {
+//     'orientation': string,
+//     'type': string,
+// }
+
+// export interface IScaleSettings {
+//     'displayed': boolean,
+//     'minValue': number,
+//     'maxValue': number,
+// }
