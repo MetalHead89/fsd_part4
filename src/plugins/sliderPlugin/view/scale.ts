5d7e05aa0ef622b9ae3e55f84fc88948a05374e0
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-12-06 16:01:51 +0700

    Added comments to the Scale class

--- src/plugins/sliderPlugin/view/scale.ts
+++ src/plugins/sliderPlugin/view/scale.ts
@@ -3,10 +3,15 @@ import { ICursorPsition } from '../interfaces';
 
 import Observer from "../observer/observer";
 
+
+/**
+ * Класс шкалы. Содержит HTML элемент шкалы и организовывает управление ею
+ */
 class Scale {
     private element: HTMLDivElement;
     private observer: Observer;
     private orientation: string;
+
 
     constructor(element: HTMLDivElement, observer: Observer) {
         this.element = element;
@@ -18,6 +23,12 @@ class Scale {
         });
     }
 
+
+    /**
+     * Возвращает максимальный размер точки шкалы
+     * 
+     * @param {IScalePointSize} maxValue - объект с шириной и высотой  последней точки шкалы
+     */
     getScalePointMaxSize(maxValue: number): IScalePointSize {
         const scalePoint = this.addScalePoint(0, 0, maxValue);
         const width = scalePoint.offsetWidth;
@@ -27,12 +38,20 @@ class Scale {
         return { 'width': width, 'height': height };
     }
 
-    addScalePoint(position: number, scalePointWidth: number, pointValue: number): HTMLElement {
 
+    /**
+     * Добавляет новую точку со значением на шкалу
+     * 
+     * @param {number} position - позиция точки со значением относительно левого или верхнего края (зависит от ориентации слайдера) родительского контейнера
+     * @param {number} scalePointWidth - ширина точки шкалы
+     * @param {number} pointValue - значение точки шкалы
+     */
+    addScalePoint(position: number, scalePointWidth: number, pointValue: number): HTMLElement {
         const scalePoint: HTMLElement = document.createElement('div');
+
         if (this.orientation === 'horizontal') {
             scalePoint.className = 'slider__scale-point slider__scale-point_horizontal';
-        } else if (this.orientation === 'vertical') {
+        } else {
             scalePoint.className = 'slider__scale-point slider__scale-point_vertical';
         }
 
@@ -40,16 +59,16 @@ class Scale {
             scalePoint.style.width = scalePointWidth + 'px';
         }
 
-        const divisionMarker: HTMLElement = document.createElement('div')
-        divisionMarker.className = ('slider__scale-point-marker')
+        const divisionMarker: HTMLElement = document.createElement('div');
+        divisionMarker.className = ('slider__scale-point-marker');
 
-        const divisionLabel: HTMLElement = document.createElement('div')
+        const divisionLabel: HTMLElement = document.createElement('div');
         if (this.orientation === 'horizontal') {
             divisionLabel.className =
-                ('slider__scale-point-label slider__scale-point-label_horizontal')
-        } else if (this.orientation === 'vertical') {
+                ('slider__scale-point-label slider__scale-point-label_horizontal');
+        } else {
             divisionLabel.className =
-                ('slider__scale-point-label slider__scale-point-label_vertical')
+                ('slider__scale-point-label slider__scale-point-label_vertical');
         }
         divisionLabel.innerText = pointValue.toString();
 
@@ -63,18 +82,34 @@ class Scale {
         scalePoint.append(divisionLabel);
         this.element.append(scalePoint);
 
-        return scalePoint
+        return scalePoint;
     }
 
+
+    /**
+     * Удаляет HTML элемент шкалы из DOM
+     */
     remove() {
         this.element.remove();
     }
 
+
+    /**
+     * Установка высоты и ширины шкалы для того, чтобы по ней можно было совершать клики, для перемещения бегунков
+     * @param {number} width - Значение ширины шкалы
+     * @param {number} height - Значение высоты шкалы
+     */
     setScaleSize(width: number, height: number): void {
         this.element.style.width = width + 'px';
         this.element.style.height = height + 'px';
     }
 
+
+    /**
+     * Возвращает позицию курсора
+     * 
+     * @param {ICursorPsition} cursorPosition - объект с позицией курсора относительно левого и верхнего края родительского контейнера
+     */
     private getPosition(cursorPosition: ICursorPsition): ICursorPsition {
 
         const parrent: HTMLElement | null = this.element.parentElement;
