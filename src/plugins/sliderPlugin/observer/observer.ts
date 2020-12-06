917c5d6f62dfc5e84c0f4f3b656fdafc4b56d3f9
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-12-06 15:23:20 +0700

    Changed the structure of the code of the Observer class

--- src/plugins/sliderPlugin/observer/observer.ts
+++ src/plugins/sliderPlugin/observer/observer.ts
@@ -1,5 +1,9 @@
 import { IObservable } from '../interfaces';
 
+
+/**
+ * Создаёт механизм подписки, позволяющий одним объектам следить и реагировать на события, происходящие в других объектах
+ */
 class Observable {
 
     // объект хранящий уведомления в виде ключей и связанные с ними функции в виде значений
@@ -9,44 +13,39 @@ class Observable {
         this.observables = {};
     }
 
+
+    /**
+     * Подписывет наблюдателя на выполнение функции func при получении уведомления notification
+     * 
+     * @param {string} notification - уведомление при получении которого вызывается функция func
+     * @param {Function} func - функция, которая вызывается после получения наблюдателем уведомления notification
+     */
     subscribe(notification: string, func: Function): void {
-
-        /**
-         * Подписывет наблюдателя на выполнение функции func при получении уведомления notification
-         * 
-         * @param {string} notification - уведомление при получении которого вызывается функция func
-         * @param {Function} func - функция, которая вызывается после получения наблюдателем уведомления notification
-         */
-
         this.observables[notification] = this.observables[notification] || [];
         this.observables[notification].push(func);
-
     }
 
+
+    /**
+     * Отписывает наблюдателя от выполнения функции func при получении уведомления notification
+     * 
+     * @param {string} notification - уведомление из которого нужно удалить обработчик func
+     * @param {Function} func - функция, которую необходимо удалить из уведомления notification
+     */
     unsubscribe(notification: string, func: Function): void {
-
-        /**
-         * Отписывает наблюдателя от выполнения функции func при получении уведомления notification
-         * 
-         * @param {string} notification - уведомление из которого нужно удалить обработчик func
-         * @param {Function} func - функция, которую необходимо удалить из уведомления notification
-         */
-
         this.observables[notification] = this.observables[notification].filter(subscriberfunc => subscriberfunc !== func);
-
     }
 
+
+    /**
+     * Отправляет наблюдателю уведомление notification, при получении которого будет запущена привязанная к нему
+     * функция с аргументом data
+     * 
+     * @param {string} notification - уведомление на которое должен среагировать наблюдатель
+     * @param {any} data - аргумент, который будет передан в функцию, вызываемую после получения
+     * наблюдателем уведомления notification
+     */
     notify(notification: string, data: any): void {
-
-        /**
-         * Отправляет наблюдателю уведомление notification, при получении которого будет запущена привязанная к нему
-         * функция с аргументом data
-         * 
-         * @param {string} notification - уведомление на которое должен среагировать наблюдатель
-         * @param {any} data - аргумент, который будет передан в функцию, вызываемую после получения
-         * наблюдателем уведомления notification
-         */
-
         if (this.observables[notification]) {
             this.observables[notification].forEach(function (listener: Function) {
                 listener(data);
