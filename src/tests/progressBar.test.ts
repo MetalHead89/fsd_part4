cb7585054877468be8d76fa5e63f2390305ddb34
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2020-12-07 10:33:36 +0700

    Added tests to check set position of the progress bar

--- src/tests/progressBar.test.ts
+++ src/tests/progressBar.test.ts
@@ -2,19 +2,36 @@
  * @jest-environment jsdom
  */
 
- import { IProgressBarPosition } from '../plugins/sliderPlugin/interfaces';
+import { IProgressBarPosition } from '../plugins/sliderPlugin/interfaces';
 import ProgressBar from '../plugins/sliderPlugin/view/progressBar'
 
 const progressBarElem: HTMLDivElement = document.createElement('div');
 const progressBar: ProgressBar = new ProgressBar(progressBarElem);
 
-beforeEach( () => {
+beforeEach(() => {
     const progressBarElem: HTMLDivElement = document.createElement('div');
     const progressBar: ProgressBar = new ProgressBar(progressBarElem);
 });
 
-describe('Progress Bar', () => {
-    test('Checking for a notification about the creation of a slider', () => {
+
+describe('Get element', () => {
+    test('Should return HTML slider element', () => {
         expect(progressBar.getElement()).toBe(progressBarElem);
+    });
+});
+
+
+describe('Set Position', () => {
+    test('Should be horisontal orientaton', () => {
+        const position: IProgressBarPosition = { 'orientation': 'horizontal', 'start': 20, 'end': 20 }
+        progressBar.setPosition(position);
+
+        expect(progressBarElem.classList.contains('slider__progress-bar_horizontal')).toBe(true);
+    });
+
+    test('Should be vertical orientaton', () => {
+        const position: IProgressBarPosition = { 'orientation': 'vertical', 'start': 20, 'end': 20 }
+        progressBar.setPosition(position);
+        expect(progressBarElem.classList.contains('slider__progress-bar_vertical')).toBe(true);
     });
 });
