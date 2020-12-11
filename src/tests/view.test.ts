d541288b48d98d7f3826c0e396398827fb6fb0f8
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2020-12-11 16:14:03 +0700

    Added tests for createTrack method from View class

--- src/tests/view.test.ts
+++ src/tests/view.test.ts
@@ -16,8 +16,11 @@ beforeEach(() => {
     _this = new View(observer, sliderWrapper);
 });
 
+
 describe('Create slider', () => {
-    test('Should be eidth: 0 and height: 0', () => {
+    test('An element with a slider class must be in sliderWrapper', () => {
+        expect(sliderWrapper.querySelectorAll('.slider').length).toBe(0);
+
         observer.subscribe('sliderElementIsCreated', 
             (size: ISliderSize) => {
                 expect(size.width).toBe(0);
@@ -25,5 +28,36 @@ describe('Create slider', () => {
             }
         )
         _this.createSlider('slider');
+
+        expect(sliderWrapper.querySelectorAll('.slider').length).toBe(1);
+    });
+
+    test('An element with a slider_horizontal class must be in sliderWrapper', () => {
+        expect(sliderWrapper.querySelectorAll('.slider_horizontal').length).toBe(0);
+
+        observer.subscribe('sliderElementIsCreated', 
+            (size: ISliderSize) => {
+                expect(size.width).toBe(0);
+                expect(size.height).toBe(0);
+            }
+        )
+        _this.createSlider('slider_horizontal');
+
+        expect(sliderWrapper.querySelectorAll('.slider_horizontal').length).toBe(1);
+    });
+});
+
+
+describe('Create track', () => {
+    test('An element with a track class must be in sliderWrapper', () => {
+        expect(sliderWrapper.querySelectorAll('.track').length).toBe(0);
+        _this.createSlider('track');
+        expect(sliderWrapper.querySelectorAll('.track').length).toBe(1);
+    });
+
+    test('An element with a track_horizontal class must be in sliderWrapper', () => {
+        expect(sliderWrapper.querySelectorAll('.track_horizontal').length).toBe(0);
+        _this.createSlider('track_horizontal');
+        expect(sliderWrapper.querySelectorAll('.track_horizontal').length).toBe(1);
     });
 });
