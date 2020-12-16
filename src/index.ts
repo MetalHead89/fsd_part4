9e0dfb08550240809200f82d8053415532429ce7
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2020-12-16 10:41:51 +0700

    Added a button to add new sliders

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,23 @@ import '@/plugins/sliderPlugin/styles/style.scss'
 import '@/plugins/sliderPanelPlugin/styles/style.scss'
 import '@/fonts/fontsStyles.scss'
 
-// Поиск блоков с классом incredibleSliderPlugin и передача их плагину для добавления в них слайдеров
-const sliders = $('.slider-wrapper').incredibleSliderPlugin();
+const addNewSliderBtn = document.querySelector('.add-new-slider');
+const buttonWrapper = document.querySelector('.button-wrapper')
+if (addNewSliderBtn !== null) {
+    addNewSliderBtn.addEventListener('click', () => {
+        if (buttonWrapper !== null) {
+            const sliderWrapper = document.createElement('div');
+            sliderWrapper.classList.add('slider-wrapper');
+            buttonWrapper.before(sliderWrapper);
+            $(sliderWrapper).incredibleSliderPlugin();
+            $(sliderWrapper).sliderControlPanel();
+        }
+    });
+}
 
-sliders.each(function() {
-    $(this).sliderControlPanel();
-});
+// Поиск блоков с классом incredibleSliderPlugin и передача их плагину для добавления в них слайдеров
+// const sliders = $('.slider-wrapper').incredibleSliderPlugin();
+
+// sliders.each(function() {
+//     $(this).sliderControlPanel();
+// });
