0fe8a01930f868792cbe664a019272194c547217
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-12-31 19:05:14 +0700

    Fixed eslint warnings

--- src/plugins/sliderPanelPlugin/panelElements/inputText.ts
+++ src/plugins/sliderPanelPlugin/panelElements/inputText.ts
@@ -1,31 +1,29 @@
-import {IInputControl} from '../interfaces'
+import { IInputControl } from '../interfaces';
 
-import PanelElement from "./panelElement";
+import PanelElement from './panelElement';
 
-class InputText extends PanelElement{
+class InputText extends PanelElement {
+  private control: HTMLDivElement;
 
-    private control: HTMLDivElement;
+  constructor(input: HTMLInputElement, lblText: string, idPrefix: string) {
+    super();
 
-    constructor(input: HTMLInputElement, labelText: string, idPrefix: string) {
-        super();
+    const controlParams: IInputControl = {
+      inputElement: input,
+      id: this.generateID(idPrefix),
+      inputType: 'text',
+      inputClass: 'slider-panel__input',
+      labelText: lblText,
+      labelClass: 'slider-panel__input-text-label',
+      wrapperClass: 'slider-panel__input-text-wrapper',
+    };
 
-        const controlParams: IInputControl = {
-            'inputElement': input,
-            'id': this.generateID(idPrefix),
-            'inputType': 'text',
-            'inputClass': 'slider-panel__input',
-            'labelText': labelText,
-            'labelClass': 'slider-panel__input-text-label',
-            'wrapperClass': 'slider-panel__input-text-wrapper'
-        }
+    this.control = this.createControl(controlParams);
+  }
 
-        this.control = this.createControl(controlParams);
-    }
-
-    getControl(): HTMLDivElement {
-        return this.control;
-    }
-
+  getControl(): HTMLDivElement {
+    return this.control;
+  }
 }
 
-export default InputText
+export default InputText;
