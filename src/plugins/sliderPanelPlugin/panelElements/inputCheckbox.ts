428eea02bcfd713c08f27e4dad87fc57c468c34a
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-12-31 17:45:18 +0700

    Fixed eslint warnings

--- src/plugins/sliderPanelPlugin/panelElements/inputCheckbox.ts
+++ src/plugins/sliderPanelPlugin/panelElements/inputCheckbox.ts
@@ -1,31 +1,29 @@
-import {IInputControl} from '../interfaces'
+import { IInputControl } from '../interfaces';
 
-import PanelElement from "./panelElement";
+import PanelElement from './panelElement';
 
-class InputCheckbox extends PanelElement{
+class InputCheckbox extends PanelElement {
+  private control: HTMLDivElement;
 
-    private control: HTMLDivElement;
+  constructor(input: HTMLInputElement, lblText: string, idPrefix: string) {
+    super();
 
-    constructor(input: HTMLInputElement, labelText: string, idPrefix: string) {
-        super();      
-    
-            const controlParams: IInputControl = {
-                'inputElement': input,
-                'id': this.generateID(idPrefix),
-                'inputType': 'checkbox',
-                'inputClass': 'slider-panel__checkbox',
-                'labelText': labelText,
-                'labelClass': 'slider-panel__input-checkbox-label',
-                'wrapperClass': 'slider-panel__input-checkbox-wrapper'
-            }
-    
-            this.control = this.createControl(controlParams);
-    }
+    const controlParams: IInputControl = {
+      inputElement: input,
+      id: this.generateID(idPrefix),
+      inputType: 'checkbox',
+      inputClass: 'slider-panel__checkbox',
+      labelText: lblText,
+      labelClass: 'slider-panel__input-checkbox-label',
+      wrapperClass: 'slider-panel__input-checkbox-wrapper',
+    };
 
-    getControl(): HTMLDivElement {
-        return this.control;
-    }
+    this.control = this.createControl(controlParams);
+  }
 
+  getControl(): HTMLDivElement {
+    return this.control;
+  }
 }
 
-export default InputCheckbox
+export default InputCheckbox;
