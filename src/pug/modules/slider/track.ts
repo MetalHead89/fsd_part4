063d24688a9d40e03463b506a8e5ae08553e1628
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-12-31 12:05:52 +0700

    Removed pug directory

--- src/pug/modules/slider/track.ts
+++
@@ -1,8 +0,0 @@
-export class Track {
-    private color: string = '';
-    private element: HTMLElement | null;
-
-    constructor(track: HTMLElement | null) {
-        this.element = track;
-    }
-}
