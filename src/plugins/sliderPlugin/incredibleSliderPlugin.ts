02a16e349a96f45f698b0af899277bc3e43448f3
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-01-02 17:33:02 +0700

    Fixed some eslinter warnings in incredibleSliderPlugin.ts

--- src/plugins/sliderPlugin/incredibleSliderPlugin.ts
+++ src/plugins/sliderPlugin/incredibleSliderPlugin.ts
@@ -1,100 +1,92 @@
+/* eslint wrap-iife: [2, "inside"] */
+
 import { ISliderSettings } from './interfaces';
 
 import Presenter from './presenter/presenter';
 
-(function ($) {
+(($) => {
+  // Настройки плагина по умолчанию
+  const defaultSettings: ISliderSettings = {
+    orienation: 'horizontal',
+    type: 'range',
+    scale: true,
+    tooltips: true,
+    min: 0,
+    max: 10,
+    step: 1,
+  };
 
-    // Настройки плагина по умолчанию
-    const defaultSettings: ISliderSettings = {
-        'orienation': 'horizontal',
-        'type': 'range',
-        'scale': true,
-        'tooltips': true,       
-        'min': 0,
-        'max': 10,
-        'step': 1
-    };
+  // API мотоды плагина
+  const methods: any = {
+    init(options: ISliderSettings) {
+      // Обновление настроек плагина в соответсвии с полученными параметрами
+      const settings: ISliderSettings = $.extend(defaultSettings, options);
 
-    // API мотоды плагина
-    const methods: any = {
+      // Создание слайдеров
+      return this.each(function createSlider(this: any) {
+        const sliderPresenter: Presenter = new Presenter(settings, this);
 
-        init: function (options: ISliderSettings) {
+        $(this).data('settings', settings);
+        $(this).data('sliderPresenter', sliderPresenter);
+      });
+    },
+    setMin(value: number) {
+      $(this).data().sliderPresenter.changeMinValue(value);
+    },
+    getMin() {
+      return $(this).data().sliderPresenter.getSliderMinValue();
+    },
+    setMax(value: number) {
+      $(this).data().sliderPresenter.changeMaxValue(value);
+    },
+    getMax() {
+      return $(this).data().sliderPresenter.getSliderMaxValue();
+    },
+    getStep() {
+      return $(this).data().sliderPresenter.getSliderStep();
+    },
+    setStep(value: number) {
+      $(this).data().sliderPresenter.changeStep(value);
+    },
+    changeScaleVisibility(flag: boolean) {
+      $(this).data().sliderPresenter.changeScaleVisibility(flag);
+    },
+    changeTooltipsVisibility(flag: boolean) {
+      $(this).data().sliderPresenter.changeTooltipsVisibility(flag);
+    },
+    getScaleVisiblity(): boolean {
+      return $(this).data().sliderPresenter.getScaleVisiblity();
+    },
+    getTooltipsVisiblity(): boolean {
+      return $(this).data().sliderPresenter.getTooltipsVisiblity();
+    },
+    changeSliderType(type: string) {
+      return $(this).data().sliderPresenter.changeSliderType(type);
+    },
+    getSliderType(): string {
+      return $(this).data().sliderPresenter.getSliderType();
+    },
+    getSliderOrientation(): string {
+      return $(this).data().sliderPresenter.getSliderOrientation();
+    },
+    changeSliderOrientation(orientation: string): void {
+      return $(this).data().sliderPresenter.changeSliderOrientation(orientation);
+    },
+  };
 
-            // Обновление настроек плагина в соответсвии с полученными параметрами
-            const settings: ISliderSettings = $.extend(defaultSettings, options);
+  $.fn.incredibleSliderPlugin = function plug(action?: string
+    | ISliderSettings, args?): JQuery<HTMLElement> {
+    let method: any;
 
-            // Создание слайдеров
-            return this.each(function (this: any) {
-                // const observer = new Observable();
-                // const model: Model = new Model(observer, settings);
-                // const view: View = new View(observer, this,
-                //     {'type': settings.type, 'orientation': settings.orienation});
-                const sliderPresenter: Presenter = new Presenter(settings, this);
+    if (typeof action === 'string' && methods[action]) {
+      method = methods[action].call(this, args);
+    } else if (typeof action === 'object' || !action) {
+      method = methods.init.call(this, args);
+    } else {
+      $.error(`Метод с именем ${action} не существует для jQuery.incredibleSliderPlugin`);
+      method = this;
+    }
 
-                $(this).data('settings', settings); ///////////////////////// добавить проверку на существование объекта
-                $(this).data('sliderPresenter', sliderPresenter);
-                // $(this).data('slider', {
-                //     target: $(this),
-                //     m: model
-                // });
-            });
-        },
-        setMin: function(value: number) {
-            $(this).data().sliderPresenter.changeMinValue(value);
-        },
-        getMin: function() {
-            return $(this).data().sliderPresenter.getSliderMinValue();
-        },
-        setMax: function(value: number) {
-            $(this).data().sliderPresenter.changeMaxValue(value);
-        },
-        getMax: function() {
-            return $(this).data().sliderPresenter.getSliderMaxValue();
-        },
-        getStep: function() {
-            return $(this).data().sliderPresenter.getSliderStep();
-        },
-        setStep: function(value: number) {
-            $(this).data().sliderPresenter.changeStep(value);
-        },
-        changeScaleVisibility: function(flag: boolean) {
-            $(this).data().sliderPresenter.changeScaleVisibility(flag);
-        },
-        changeTooltipsVisibility: function(flag: boolean) {
-            $(this).data().sliderPresenter.changeTooltipsVisibility(flag);
-        },
-        getScaleVisiblity: function(): boolean {
-            return $(this).data().sliderPresenter.getScaleVisiblity();
-        },
-        getTooltipsVisiblity: function(): boolean {
-            return $(this).data().sliderPresenter.getTooltipsVisiblity();
-        },
-        changeSliderType: function(type: string) {
-            return $(this).data().sliderPresenter.changeSliderType(type);
-        },
-        getSliderType: function(): string {
-            return $(this).data().sliderPresenter.getSliderType();
-        },
-        getSliderOrientation: function(): string {
-            return $(this).data().sliderPresenter.getSliderOrientation();
-        },
-        changeSliderOrientation: function(orientation: string): void {
-            return $(this).data().sliderPresenter.changeSliderOrientation(orientation);
-        }
-        
-    };
-
-    $.fn.incredibleSliderPlugin = function (action?: string | ISliderSettings, args?): JQuery<HTMLElement> {
-
-        if (typeof action === 'string' && methods[action]) {
-            return methods[action].call(this, args);
-        } else if (typeof action === 'object' || !action) {
-            return methods.init.call(this, args);
-        } else {
-            $.error('Метод с именем ' + action + ' не существует для jQuery.incredibleSliderPlugin');
-            return this;
-        }
-
-    };
-
+    return method;
+  };
 })(jQuery);
