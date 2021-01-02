b8aea482942a1e767ca02a507837743f89612a45
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-01-02 12:13:34 +0700

    Fixed eslint warnings in track.ts

--- src/plugins/sliderPlugin/view/track.ts
+++ src/plugins/sliderPlugin/view/track.ts
@@ -1,60 +1,54 @@
 import { ICursorPosition } from '../interfaces';
 
-import Observer from "../observer/observer";
-
+import Observer from '../observer/observer';
 
 /**
  * Класс дорожки слайдера. Содержит HTML элемент дорожки слайдера и организовывает управление им
  */
 class Track {
-    private element: HTMLDivElement;
-    private observer: Observer;
+  private element: HTMLDivElement;
+  private observer: Observer;
 
+  constructor(element: HTMLDivElement, observer: Observer) {
+    this.element = element;
+    this.observer = observer;
+    this.addClickEventListener();
+  }
 
-    constructor(element: HTMLDivElement, observer: Observer) {
-        this.element = element;
-        this.observer = observer;
-        this.addClickEventListener();
+  /**
+   * Добавляет событие на клик по дорожке слайдера
+   */
+  private addClickEventListener(): void {
+    this.element.addEventListener('click', this.clickToTrack.bind(this));
+  }
+
+  /**
+   * Обработка клика по дорожке. Метод отправляет уведомление с координатами курсора
+   * @param {MouseEvent} event - объект события клика
+   */
+  private clickToTrack(event: MouseEvent): void {
+    this.observer.notify('clickOnTheTrack', this.getPosition({ x: event.clientX, y: event.clientY }));
+  }
+
+  /**
+   * Возвращает позицию курсора
+   * @param {ICursorPosition} cursorPosition - объект с позицией курсора
+   * относительно левого и верхнего края экрана
+   * @returns {ICursorPosition} - объект с позицией курсора относительно
+   * левого и верхнего края родительского контейнера
+   */
+  private getPosition(cursorPosition: ICursorPosition): ICursorPosition {
+    const positionInsideParent: ICursorPosition = { ...cursorPosition };
+    const parrent: HTMLElement | null = this.element.parentElement;
+
+    if (parrent !== null) {
+      const parrentCoords: DOMRect = parrent.getBoundingClientRect();
+      positionInsideParent.x -= parrentCoords.left;
+      positionInsideParent.y -= parrentCoords.top;
     }
 
-
-    /**
-     * Добавляет событие на клик по дорожке слайдера
-     */
-    private addClickEventListener(): void {
-        this.element.addEventListener('click', this.clickToTrack.bind(this));
-    }
-
-
-    /**
-     * Обработка клика по дорожке. Метод отправляет уведомление с координатами курсора
-     * @param {MouseEvent} event - объект события клика 
-     */
-    private clickToTrack(event: MouseEvent): void {
-        this.observer.notify('clickOnTheTrack', this.getPosition({ 'x': event.clientX, 'y': event.clientY }));
-    }
-
-
-    /**
-     * Возвращает позицию курсора
-     * 
-     * @param {ICursorPosition} cursorPosition - объект с позицией курсора относительно левого и верхнего края экрана
-     * 
-     * @returns {ICursorPosition} - объект с позицией курсора относительно левого и верхнего края родительского контейнера
-     */
-    private getPosition(cursorPosition: ICursorPosition): ICursorPosition {
-        const positionInsideParent: ICursorPosition = Object.assign({}, cursorPosition);
-        const parrent: HTMLElement | null = this.element.parentElement;
-
-        if (parrent !== null) {
-            const parrentCoords: DOMRect = parrent.getBoundingClientRect();
-            positionInsideParent.x = positionInsideParent.x - parrentCoords.left;
-            positionInsideParent.y = positionInsideParent.y - parrentCoords.top;
-        }
-
-        return positionInsideParent;
-    }
-
+    return positionInsideParent;
+  }
 }
 
 export default Track;
