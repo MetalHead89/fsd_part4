e9ab55baa2e81dfeac482411a0ffe6b3750a5b39
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-01-02 12:09:21 +0700

    Fixed eslint warnings in tooltip.ts

--- src/plugins/sliderPlugin/view/tooltip.ts
+++ src/plugins/sliderPlugin/view/tooltip.ts
@@ -1,62 +1,53 @@
-import { ITooltipPosition } from "../interfaces";
-import { IBorderCoords } from '../interfaces';
+import { ITooltipPosition, IBorderCoords } from '../interfaces';
 
 /**
  * Класс для управления элементами для показа значений над бегунками
  */
 class Tooltip {
-    private element: HTMLDivElement;
+  private element: HTMLDivElement;
 
+  constructor(tooltipElement: HTMLDivElement) {
+    this.element = tooltipElement;
+  }
 
-    constructor(tooltipElement: HTMLDivElement) {
-        this.element = tooltipElement;
-    }
+  /**
+   * Утанавливает новое значение над бегунком
+   * @param {Number} newValue - значение позиции на которой находится бегунок
+   */
+  setValue(newValue: number): void {
+    this.element.innerText = newValue.toString();
+  }
 
+  /**
+   * Изменяет позицию значения над бегунком
+   * @param {ITooltipPosition} position - позиция значения над бегунком
+   */
+  moveTo(position: ITooltipPosition): void {
+    this.element.style.left = `${position.left}px`;
+    this.element.style.top = `${position.top}px`;
+  }
 
-    /**
-     * Утанавливает новое значение над бегунком
-     * 
-     * @param {Number} newValue - значение позиции на которой находится бегунок
-     */
-    setValue(newValue: number): void {
-        this.element.innerText = newValue.toString();
-    }
+  /**
+   * Удаляет HTML элемент, отображающий значение позиции на которой находится бегунок
+   */
+  remove(): void {
+    this.element.remove();
+  }
 
+  /**
+   * Возвращает координаты границ значения над бегунком
+   * @returns {IBorderCoords} - Координаты границ значения над бегунком
+   */
+  getCoords(): IBorderCoords {
+    const tooltipCoords = this.element.getBoundingClientRect();
 
-    /**
-     * Изменяет позицию значения над бегунком
-     * 
-     * @param {ITooltipPosition} position - позиция значения над бегунком
-     */
-    moveTo(position: ITooltipPosition): void {
-        this.element.style.left = position.left + 'px';
-        this.element.style.top = position.top + 'px';
-    }
-
-
-    /**
-     * Удаляет HTML элемент, отображающий значение позиции на которой находится бегунок
-     */
-    remove(): void {
-        this.element.remove();
-    }
-
-
-    /**
-     * Возвращает координаты границ значения над бегунком
-     * 
-     * @returns {IBorderCoords} - Координаты границ значения над бегунком
-     */
-    getCoords(): IBorderCoords {
-        const tooltipCoords = this.element.getBoundingClientRect();
-
-        return {
-            'left': tooltipCoords.left,
-            'top': tooltipCoords.top,
-            'right': tooltipCoords.right,
-            'bottom': tooltipCoords.bottom
-        };
-    }
+    return {
+      left: tooltipCoords.left,
+      top: tooltipCoords.top,
+      right: tooltipCoords.right,
+      bottom: tooltipCoords.bottom,
+    };
+  }
 }
 
 export default Tooltip;
