c95e5bda164ef1825f0b0a0578658418e6e4ebb3
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-01-31 19:59:31 +0700

    Added class Track

--- src/plugins/simple-slider/view/slider.ts
+++ src/plugins/simple-slider/view/slider.ts
@@ -1,3 +1,14 @@
-class Slider {}
+import Track from './track';
+
+/**
+ * Класс дорожки слайдера. Содержит HTML элемент дорожки слайдера и организовывает управление им
+ */
+class Slider {
+  protected track: Track;
+
+  constructor() {
+    this.track = new Track();
+  }
+}
 
 export default Slider;
---
+++ src/plugins/simple-slider/view/track.ts
@@ -0,0 +1,10 @@
+class Track {
+  element: HTMLDivElement;
+
+  constructor() {
+    this.element = document.createElement('div');
+    this.element.classList.add('slider__track slider__track_horizontal');
+  }
+}
+
+export default Track;
