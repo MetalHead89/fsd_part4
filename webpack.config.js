259698fb0e7ffa2b7e8cc3922c722d2c9b9515fd
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-01-31 21:18:02 +0700

    Changing indentation in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -7,103 +7,109 @@ const loader = require('sass-loader');
 const webpack = require('webpack');
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: '@/index.ts',
-    devtool: 'inline-source-map',
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: '@/index.ts',
+  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
-    devServer: {
-        port: 4200
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'src/demo/index.pug')
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin([
-            { filename: '[name].css' }
-        ]),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: path.resolve(__dirname, 'src/fonts'), to: path.resolve(__dirname, 'dist/fonts') },
-                { from: path.resolve(__dirname, 'src/demo/images'), to: path.resolve(__dirname, 'dist/src/images') }
-            ]
-        })
+  },
+  devServer: {
+    port: 4200,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/demo/index.pug'),
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin([{ filename: '[name].css' }]),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/fonts'),
+          to: path.resolve(__dirname, 'dist/fonts'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/demo/images'),
+          to: path.resolve(__dirname, 'dist/src/images'),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: 'postcss.config.js' } },
+          },
+        ],
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true,
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader'],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader },
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {sourceMap: true, config: {path: 'postcss.config.js'}}
-                    }
-                ]
-            },
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader',
-                options: {
-                    pretty: true
-                }
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot)$/,
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[ext]"
-                    }
-                }]
-            },
-            {
-                test: /\.(png|jpg|svg|gif)$/,
-                use: ['file-loader']
-            }
-        ]
-    }
-}
+  },
+};
