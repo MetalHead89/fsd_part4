d6228428fba1ba3b4554089e4d6cfefde168d53d
Bakalov Vitalij <bakalov.vitaliy@gmail.com>
2021-02-01 07:39:57 +0700

    Installed default classes for the progress-bar component

--- src/plugins/simple-slider/view/progress-bar/progress-bar.ts
+++ src/plugins/simple-slider/view/progress-bar/progress-bar.ts
@@ -3,6 +3,7 @@ class ProgressBar {
 
   constructor() {
     this.element = document.createElement('div');
+    this.element.classList.add('slider__progress-bar', 'slider__progress-bar_horizontal');
   }
 
   getElement(): HTMLDivElement {
