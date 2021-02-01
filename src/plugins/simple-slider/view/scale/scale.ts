0c731ad14d72c06cd408104b4baaa67a34e5a3df
Bakalov Vitalij <bakalov.vitaliy@gmail.com>
2021-02-02 05:13:07 +0700

    Added getElement method do Scale class

--- src/plugins/simple-slider/view/scale/scale.ts
+++ src/plugins/simple-slider/view/scale/scale.ts
@@ -4,6 +4,10 @@ class Scale {
   constructor() {
     this.element = document.createElement('div');
   }
+
+  getElement(): HTMLDivElement {
+    return this.element;
+  }
 }
 
 export default Scale;
