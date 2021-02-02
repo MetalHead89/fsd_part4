170ae0ac70a88a5b8d287693c9b65781d3347dfe
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-02-02 15:48:08 +0700

    Comments on the methods of the SimpleSliderModel class are written

--- src/plugins/simple-slider/model/simple-slider-model.ts
+++ src/plugins/simple-slider/model/simple-slider-model.ts
@@ -7,16 +7,27 @@ class SimpleSliderModel implements SimpleSliderModelInterface {
     this.thumbsObservers = new Array();
   }
 
+  /**
+   * Регистрация нового наблюдателя, следящего за изменением позиций бегунков
+   * @param {IThumbsObserver} observer - регистрируемый наблюдатель
+   */
   registerObserver(observer: IThumbsObserver): void {
     this.thumbsObservers.push(observer);
   }
 
+  /**
+   * Удаление наблюдателя, следящего за изменением позиций бегунков
+   * @param {IThumbsObserver} observer - удаляемый наблюдатель
+   */
   removeObserver(observer: IThumbsObserver) {
     this.thumbsObservers = this.thumbsObservers.filter((registeredObserver) => {
       registeredObserver !== observer;
     });
   }
 
+  /**
+   * Оповещение зарегистрированных наблюдателей об изменении позиций бегунков
+   */
   notifyThumbsMoveObservers() {
     this.thumbsObservers.forEach((registeredObserver) =>
       registeredObserver.updateThumbsPosition(),
