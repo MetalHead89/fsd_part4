ea40ea27e850bd901671d74a729c9242d99c0ea8
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-02-06 22:41:41 +0700

    Tests related to the Subject class have been removed from the file simple-slider-model.test.ts

--- src/tests/simple-slider-model.test.ts
+++ src/tests/simple-slider-model.test.ts
@@ -1,4 +1,5 @@
 /* eslint-disable dot-notation */
+/* eslint-disable comma-dangle */
 
 import SimpleSliderModel from '../plugins/simple-slider/model/simple-slider-model';
 
@@ -7,115 +8,6 @@ let model = new SimpleSliderModel();
 beforeEach(() => {
   model = new SimpleSliderModel();
 });
-
-// describe('Register observer', () => {
-//   test('Observers must be added', () => {
-//     expect(model['thumbsObservers'].length).toBe(0);
-
-//     const newObserverOne = {
-//       updateThumbsPosition(): void {
-//         /* some operations */
-//       },
-//     };
-//     const newObserverTwo = {
-//       updateThumbsPosition(): void {
-//         /* some operations */
-//       },
-//     };
-//     const newObserverThree = {
-//       updateThumbsPosition(): void {
-//         /* some operations */
-//       },
-//     };
-
-//     model.registerObserver(newObserverOne);
-//     expect(model['thumbsObservers'].length).toBe(1);
-
-//     model.registerObserver(newObserverTwo);
-//     expect(model['thumbsObservers'].length).toBe(2);
-
-//     model.registerObserver(newObserverThree);
-//     expect(model['thumbsObservers'].length).toBe(3);
-//   });
-// });
-
-// describe('Remove observer', () => {
-//   test('Observers must be removed', () => {
-//     expect(model['thumbsObservers'].length).toBe(0);
-
-//     const newObserverOne = {
-//       updateThumbsPosition(): void {
-//         /* some operations */
-//       },
-//     };
-//     const newObserverTwo = {
-//       updateThumbsPosition(): void {
-//         /* some operations */
-//       },
-//     };
-//     const newObserverThree = {
-//       updateThumbsPosition(): void {
-//         /* some operations */
-//       },
-//     };
-
-//     model.registerObserver(newObserverOne);
-//     model.registerObserver(newObserverTwo);
-//     model.registerObserver(newObserverThree);
-
-//     expect(model['thumbsObservers'].length).toBe(3);
-
-//     model.removeObserver(newObserverOne);
-//     expect(model['thumbsObservers'].length).toBe(2);
-
-//     model.removeObserver(newObserverTwo);
-//     expect(model['thumbsObservers'].length).toBe(1);
-
-//     model.removeObserver(newObserverThree);
-//     expect(model['thumbsObservers'].length).toBe(0);
-//   });
-// });
-
-// describe('Notify Thumbs Move Observers', () => {
-//   test('Observers must be notified', () => {
-//     let numberOfNotifiedObservers = 0;
-
-//     const newObserverOne = {
-//       updateThumbsPosition(): void {
-//         numberOfNotifiedObservers += 1;
-//       },
-//     };
-//     const newObserverTwo = {
-//       updateThumbsPosition(): void {
-//         numberOfNotifiedObservers += 1;
-//       },
-//     };
-//     const newObserverThree = {
-//       updateThumbsPosition(): void {
-//         numberOfNotifiedObservers += 1;
-//       },
-//     };
-
-//     model.registerObserver(newObserverOne);
-//     model['notifyThumbsMoveObservers']();
-//     expect(numberOfNotifiedObservers).toBe(1);
-
-//     numberOfNotifiedObservers = 0;
-//     model.registerObserver(newObserverTwo);
-//     model['notifyThumbsMoveObservers']();
-//     expect(numberOfNotifiedObservers).toBe(2);
-
-//     numberOfNotifiedObservers = 0;
-//     model.registerObserver(newObserverThree);
-//     model['notifyThumbsMoveObservers']();
-//     expect(numberOfNotifiedObservers).toBe(3);
-
-//     numberOfNotifiedObservers = 0;
-//     model.removeObserver(newObserverThree);
-//     model['notifyThumbsMoveObservers']();
-//     expect(numberOfNotifiedObservers).toBe(2);
-//   });
-// });
 
 describe('Set slider size', () => {
   test('Slider width should be 5 and height should be 9', () => {
@@ -240,7 +132,7 @@ describe('Get thumbs positions', () => {
       expect(pos.thumbOne.top).toBe(0);
       expect(pos.thumbTwo.left).toBeCloseTo(294);
       expect(pos.thumbTwo.top).toBe(0);
-    },
+    }
   );
 
   test(
@@ -258,7 +150,7 @@ describe('Get thumbs positions', () => {
       expect(pos.thumbOne.top).toBe(0);
       expect(pos.thumbTwo.left).toBeCloseTo(0);
       expect(pos.thumbTwo.top).toBe(420);
-    },
+    }
   );
 });
 
