41822057c860db3b8ca41a27da56c2b2d819b6bc
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-02-07 17:45:43 +0700

    Added methods for moving the thumb to the Thumb class

--- src/plugins/simple-slider/view/thumb/thumb.ts
+++ src/plugins/simple-slider/view/thumb/thumb.ts
@@ -1,18 +1,127 @@
-import { IThumbPosition, ISize } from '../../interfaces';
+/* eslint operator-linebreak: ["error", "after"] */
+/* eslint-disable comma-dangle */
 
-class Thumb {
+import { IPosition, ISize } from '../../interfaces';
+import Subject from '../../subject/subject';
+
+class Thumb extends Subject {
   private element: HTMLDivElement;
+  private shift = { shiftX: 0, shiftY: 0 };
+  private onMouseMoveHandler = this.drag.bind(this);
+  private onMouseUpHandler = this.endDrag.bind(this);
+  private position = { left: 0, top: 0 };
 
   constructor() {
+    super();
     this.element = document.createElement('div');
     this.element.classList.add('slider__thumb', 'slider__thumb_horizontal');
+    this.addMousedownEventListener();
+    this.disableDragAndDrop();
   }
 
   getElement(): HTMLDivElement {
     return this.element;
   }
 
-  moveTo(position: IThumbPosition): void {
+  /**
+   * Отключает html5 Drag and Drop
+   */
+  private disableDragAndDrop(): void {
+    this.element.ondragstart = () => false;
+  }
+
+  /**
+   * Добавляет событие на нажатие кнопки мыши на бегунке
+   */
+  private addMousedownEventListener(): void {
+    this.element.addEventListener('mousedown', this.clickToThumb.bind(this));
+  }
+
+  /**
+   * Обработка перемещения мыши
+   * @param {MouseEvent} event - объект события клика
+   */
+  private clickToThumb(event: MouseEvent): void {
+    const thumbCoords: DOMRect = this.element.getBoundingClientRect();
+
+    this.shift.shiftX = event.clientX - thumbCoords.left;
+    this.shift.shiftY = event.clientY - thumbCoords.top;
+
+    document.addEventListener('mousemove', this.onMouseMoveHandler);
+    document.addEventListener('mouseup', this.onMouseUpHandler);
+
+    document.addEventListener('touchmove', this.onMouseMoveHandler);
+    document.addEventListener('touchend', this.onMouseUpHandler);
+  }
+
+  /**
+   *  Оповещает подписчиков о том, что бегунок пытаются переместить
+   */
+  private drag(event: MouseEvent | TouchEvent): void {
+    if (event instanceof MouseEvent) {
+      this.position = this.getPosition({
+        left: event.clientX,
+        top: event.clientY,
+      });
+    } else {
+      this.position = this.getPosition({
+        left: event.targetTouches[0].pageX,
+        top: event.targetTouches[0].pageY,
+      });
+    }
+
+    this.notify('thumbIsDragged');
+  }
+
+  /**
+   * Возвращает позицию курсора
+   * @param {ICursorPosition} cursorPosition - объект с позицией курсора
+   * относительно левого и верхнего края экрана
+   * @returns {ICursorPosition} - объект с позицией курсора относительно
+   * левого и верхнего края родительского контейнера
+   */
+  private getPosition(cursorPosition: IPosition): IPosition {
+    const positionInsideParent: IPosition = {
+      left: cursorPosition.left,
+      top: cursorPosition.top,
+    };
+
+    const parrent: HTMLElement | null = this.element.parentElement;
+
+    if (parrent !== null) {
+      const parrentCoords: DOMRect = parrent.getBoundingClientRect();
+      positionInsideParent.left =
+        positionInsideParent.left - parrentCoords.left - this.shift.shiftX;
+      positionInsideParent.top =
+        positionInsideParent.top - parrentCoords.top - this.shift.shiftY;
+    }
+
+    return positionInsideParent;
+  }
+
+  /**
+   * Завершает передвижение бегунка, удаляя слушателей событий, которые больше не нужны
+   */
+  private endDrag(): void {
+    document.removeEventListener(
+      'mousemove',
+      this.onMouseMoveHandler as EventListenerOrEventListenerObject
+    );
+    document.removeEventListener(
+      'mouseup',
+      this.onMouseUpHandler as EventListenerOrEventListenerObject
+    );
+    document.removeEventListener(
+      'touchmove',
+      this.onMouseMoveHandler as EventListenerOrEventListenerObject
+    );
+    document.removeEventListener(
+      'touchend',
+      this.onMouseUpHandler as EventListenerOrEventListenerObject
+    );
+  }
+
+  moveTo(position: IPosition): void {
     this.element.style.left = `${position.left}px`;
     this.element.style.top = `${position.top}px`;
   }
