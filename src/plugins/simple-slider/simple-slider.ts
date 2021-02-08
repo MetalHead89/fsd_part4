ef34d563e8f4b27faff9baee3629582837b4ac8a
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-02-08 16:08:35 +0700

    Added jQuery plugin wrapper to simple-slider.ts

--- src/plugins/simple-slider/simple-slider.ts
+++ src/plugins/simple-slider/simple-slider.ts
@@ -1,108 +1,67 @@
-import { ISimpleSliderModel, ISimpleSliderView } from './interfaces';
+import { ISimpleSliderModel, ISimpleSliderView, ISliderSettings } from './interfaces';
 
 import SimpleSliderModel from './model/simple-slider-model';
 import SimpleSliderController from './controller/simple-slider-controller';
 import SimpleSliderView from './view/simple-slider-view';
 
-// Временный способ создания слайдера
-const simpleSliderModel: ISimpleSliderModel = new SimpleSliderModel();
-const simpleSliderView: ISimpleSliderView = new SimpleSliderView();
-const simpleSliderController = new SimpleSliderController(
-  simpleSliderModel,
-  simpleSliderView,
-);
+// // Временный способ создания слайдера
+// const simpleSliderModel: ISimpleSliderModel = new SimpleSliderModel();
+// const simpleSliderView: ISimpleSliderView = new SimpleSliderView();
+// const simpleSliderController = new SimpleSliderController(
+//   simpleSliderModel,
+//   simpleSliderView,
+// );
 
-// const simpleSliderView = new SimpleSliderView(simpleSliderModel);
+(($) => {
+  // Настройки плагина по умолчанию
+  const defaultSettings = {
+    orienation: 'horizontal',
+    type: 'range',
+    scale: true,
+    tooltips: true,
+    min: 0,
+    max: 10,
+    step: 1,
+    thumbOneValue: 3,
+    thumbTwoValue: 7,
+  };
 
-// /* eslint wrap-iife: [2, "inside"] */
+  // API мотоды плагина
+  const methods: any = {
+    init(options: ISliderSettings) {
+      // Обновление настроек плагина в соответсвии с полученными параметрами
+      const settings: ISliderSettings = $.extend(defaultSettings, options);
 
-// import { ISliderSettings } from './interfaces';
+      // Создание слайдеров
+      return this.each(function createSlider(this: any) {
+        console.log('dfsf')
+        const model = new SimpleSliderModel();
+        const view = new SimpleSliderView();
+        const controller = new SimpleSliderController(model, view);
 
-// import Presenter from './presenter/presenter';
+        $(this).data('settings', settings);
+        $(this).data('controller', controller);
+      });
+    }
+  };
 
-// (($) => {
-//   // Настройки плагина по умолчанию
-//   const defaultSettings: ISliderSettings = {
-//     orienation: 'horizontal',
-//     type: 'range',
-//     scale: true,
-//     tooltips: true,
-//     min: 0,
-//     max: 10,
-//     step: 1,
-//   };
+  $.fn.simpleSlider = function plug(
+    action?: string | ISliderSettings,
+    args?,
+  ): JQuery<HTMLElement> {
+    let method: any;
 
-//   // API мотоды плагина
-//   const methods: any = {
-//     init(options: ISliderSettings) {
-//       // Обновление настроек плагина в соответсвии с полученными параметрами
-//       const settings: ISliderSettings = $.extend(defaultSettings, options);
+    if (typeof action === 'string' && methods[action]) {
+      method = methods[action].call(this, args);
+    } else if (typeof action === 'object' || !action) {
+      method = methods.init.call(this, args);
+    } else {
+      $.error(
+        `Метод с именем ${action} не существует для jQuery.simpleSlider`,
+      );
+      method = this;
+    }
 
-//       // Создание слайдеров
-//       return this.each(function createSlider(this: any) {
-//         const sliderPresenter: Presenter = new Presenter(settings, this);
-
-//         $(this).data('settings', settings);
-//         $(this).data('sliderPresenter', sliderPresenter);
-//       });
-//     },
-//     setMin(value: number) {
-//       $(this).data().sliderPresenter.changeMinValue(value);
-//     },
-//     getMin() {
-//       return $(this).data().sliderPresenter.getSliderMinValue();
-//     },
-//     setMax(value: number) {
-//       $(this).data().sliderPresenter.changeMaxValue(value);
-//     },
-//     getMax() {
-//       return $(this).data().sliderPresenter.getSliderMaxValue();
-//     },
-//     getStep() {
-//       return $(this).data().sliderPresenter.getSliderStep();
-//     },
-//     setStep(value: number) {
-//       $(this).data().sliderPresenter.changeStep(value);
-//     },
-//     changeScaleVisibility(flag: boolean) {
-//       $(this).data().sliderPresenter.changeScaleVisibility(flag);
-//     },
-//     changeTooltipsVisibility(flag: boolean) {
-//       $(this).data().sliderPresenter.changeTooltipsVisibility(flag);
-//     },
-//     getScaleVisiblity(): boolean {
-//       return $(this).data().sliderPresenter.getScaleVisiblity();
-//     },
-//     getTooltipsVisiblity(): boolean {
-//       return $(this).data().sliderPresenter.getTooltipsVisiblity();
-//     },
-//     changeSliderType(type: string) {
-//       return $(this).data().sliderPresenter.changeSliderType(type);
-//     },
-//     getSliderType(): string {
-//       return $(this).data().sliderPresenter.getSliderType();
-//     },
-//     getSliderOrientation(): string {
-//       return $(this).data().sliderPresenter.getSliderOrientation();
-//     },
-//     changeSliderOrientation(orientation: string): void {
-//       return $(this).data().sliderPresenter.changeSliderOrientation(orientation);
-//     },
-//   };
-
-//   $.fn.incredibleSliderPlugin = function plug(action?: string
-//     | ISliderSettings, args?): JQuery<HTMLElement> {
-//     let method: any;
-
-//     if (typeof action === 'string' && methods[action]) {
-//       method = methods[action].call(this, args);
-//     } else if (typeof action === 'object' || !action) {
-//       method = methods.init.call(this, args);
-//     } else {
-//       $.error(`Метод с именем ${action} не существует для jQuery.incredibleSliderPlugin`);
-//       method = this;
-//     }
-
-//     return method;
-//   };
-// })(jQuery);
+    return method;
+  };
+})(jQuery);
