dea45e4abd59eabc52d98c969acccdc9a17b5b37
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-02-10 07:47:41 +0700

    Added update mathod to ProgressBar class

--- src/plugins/simple-slider/view/progress-bar/progress-bar.ts
+++ src/plugins/simple-slider/view/progress-bar/progress-bar.ts
@@ -1,13 +1,25 @@
+import { IProgressBarParams } from '../../interfaces';
+
 class ProgressBar {
   private element: HTMLDivElement;
 
   constructor() {
     this.element = document.createElement('div');
-    this.element.classList.add('slider__progress-bar', 'slider__progress-bar_horizontal');
+    this.element.classList.add(
+      'slider__progress-bar',
+      'slider__progress-bar_horizontal',
+    );
   }
 
   getElement(): HTMLDivElement {
     return this.element;
+  }
+
+  update(params: IProgressBarParams): void {
+    this.element.style.left = `${params.position.left}px`;
+    this.element.style.top = `${params.position.top}px`;
+    this.element.style.width = `${params.size.width}px`;
+    this.element.style.height = `${params.size.height}px`;
   }
 }
 
