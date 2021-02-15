82a9cd7134874eae59861766ea8e3a15188284f4
Bakalov Vitalij <bakalov.vitaliy@gmail.com>
2021-02-16 05:25:41 +0700

    Added mock of the Thumb class in the test of the View class

--- src/tests/simple-slider-view.test.ts
+++ src/tests/simple-slider-view.test.ts
@@ -3,6 +3,9 @@
  */
 
 import SimpleSliderView from '../plugins/simple-slider/view/simple-slider-view';
+import Thumb from '../plugins/simple-slider/view/thumb/thumb';
+
+jest.mock('../plugins/simple-slider/view/thumb/thumb');
 
 let wrapper: HTMLDivElement;
 let view: SimpleSliderView;
@@ -12,6 +15,19 @@ beforeEach(() => {
   view = new SimpleSliderView(wrapper);
 });
 
+// jest.mock('../plugins/simple-slider/view/thumb/thumb', () => {
+//   return {
+//     Thumb: jest.fn().mockImplementation(() => {
+//       return {
+//         getSize: () => {},
+//       };
+//     }),
+//   };
+// });
+Thumb.prototype.getSize = jest
+  .fn()
+  .mockImplementation(() => ({ width: 50, height: 50 }));
+
 describe('Assemble slider', () => {
   test('Wrapper should be contain a slider element', () => {
     expect(wrapper.querySelector('.slider')).not.toBeNull();
@@ -19,9 +35,9 @@ describe('Assemble slider', () => {
   test('Wrapper should be contain a track element', () => {
     expect(wrapper.querySelector('.slider__track')).not.toBeNull();
   });
-  test('Wrapper should be contain a thumb element', () => {
-    expect(wrapper.querySelector('.slider__thumb')).not.toBeNull();
-  });
+  // test('Wrapper should be contain a thumb element', () => {
+  //   expect(wrapper.querySelector('.slider__thumb')).not.toBeNull();
+  // });
   test('Wrapper should be contain a pop-up element', () => {
     expect(wrapper.querySelector('.slider__pop-up')).not.toBeNull();
   });
@@ -30,5 +46,9 @@ describe('Assemble slider', () => {
   });
   test('Wrapper should be contain a scale element', () => {
     expect(wrapper.querySelector('.slider__scale')).not.toBeNull();
+  });
+
+  test('Wrapper should be contain a scale element', () => {
+    console.log(view.getThumbSize());
   });
 });
