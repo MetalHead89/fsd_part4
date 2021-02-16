a517155e28561027da127203780ee1aff1892df5
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-02-16 14:13:56 +0700

    Added manual mock of class thumb to the file with tests of the SimpleSliderView class

---
+++ src/plugins/simple-slider/view/thumb/__mocks__/thumb.ts
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+export default jest.fn(() => {
+  return {
+    register: () => true,
+    getElement: () => {
+      const div = document.createElement('div');
+      div.classList.add('slider__thumb');
+      return div;
+    },
+    getSize: () => true,
+  };
+});
