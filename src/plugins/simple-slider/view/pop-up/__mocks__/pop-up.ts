65bb9a08d424a8117f1fd7b92abd8883ede50eae
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-02-16 16:45:13 +0700

    Added popup class mock

---
+++ src/plugins/simple-slider/view/pop-up/__mocks__/pop-up.ts
@@ -0,0 +1,10 @@
+/* eslint-disable arrow-body-style */
+export default jest.fn(() => {
+  return {
+    getElement: () => {
+      const div = document.createElement('div');
+      div.classList.add('slider__pop-up');
+      return div;
+    },
+  };
+});
