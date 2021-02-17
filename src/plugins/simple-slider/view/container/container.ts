fb5a717141e65246c3d7a63541836c62b0323847
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-02-17 20:13:46 +0700

    Fixed broken slider layout

--- src/plugins/simple-slider/view/container/container.ts
+++ src/plugins/simple-slider/view/container/container.ts
@@ -3,7 +3,7 @@ import Element from '../element/element';
 class Container extends Element {
   constructor() {
     super();
-    this.element.classList.add('slider');
+    this.element.classList.add('slider', 'slider_horizontal');
   }
 
   append(control: HTMLDivElement): void {
