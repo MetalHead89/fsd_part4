b33e48ff36a76ec21b1498d421b71ab6e69d44ce
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-02-17 11:48:49 +0700

    The Track class extends the Element class

--- src/plugins/simple-slider/view/track/track.ts
+++ src/plugins/simple-slider/view/track/track.ts
@@ -1,13 +1,9 @@
-class Track {
-  element: HTMLDivElement;
+import Element from '../element/element';
 
+class Track extends Element {
   constructor() {
-    this.element = document.createElement('div');
+    super();
     this.element.classList.add('slider__track', 'slider__track_horizontal');
-  }
-
-  getElement(): HTMLDivElement {
-    return this.element;
   }
 }
 
