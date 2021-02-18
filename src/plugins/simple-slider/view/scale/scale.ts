46c036cdd335a83a4069248a313694a4370b249e
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-02-18 08:30:48 +0700

    Added the getPointSize method to the Scale class

--- src/plugins/simple-slider/view/scale/scale.ts
+++ src/plugins/simple-slider/view/scale/scale.ts
@@ -1,9 +1,42 @@
+import { ISize } from '../../interfaces';
 import Element from '../element/element';
 
 class Scale extends Element {
   constructor() {
     super();
-    this.element.classList.add('slider__scale');
+    this.element.classList.add('slider__scale', 'slider__scale_horizontal');
+  }
+
+  getPointSize(value: number): ISize {
+    const orienation = this.getOrientation();
+    const scalePoint: HTMLElement = document.createElement('div');
+    scalePoint.classList.add(
+      'slider__scale-point',
+      `slider__scale-point_${orienation}`,
+    );
+
+    const divisionMarker: HTMLElement = document.createElement('div');
+    divisionMarker.classList.add('slider__scale-point-marker');
+
+    const divisionLabel: HTMLElement = document.createElement('div');
+    divisionLabel.classList.add(
+      'slider__scale-point-label',
+      `slider__scale-point-label_${orienation}`,
+    );
+    divisionLabel.innerText = value.toString();
+
+    scalePoint.append(divisionMarker);
+    scalePoint.append(divisionLabel);
+    this.element.append(scalePoint);
+
+    const pointSize = {
+      width: scalePoint.offsetWidth,
+      height: scalePoint.offsetHeight,
+    };
+
+    scalePoint.remove();
+
+    return pointSize;
   }
 }
 
