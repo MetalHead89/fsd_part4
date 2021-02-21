c65cbf7c492bbf109bbd73a118d97e752b67f85a
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-02-21 14:44:15 +0700

    Added a group of elements for input / output of the minimum and maximum values ​​of thumbs, as well as the step value

--- src/plugins/control-panel/view/control-panel-view.ts
+++ src/plugins/control-panel/view/control-panel-view.ts
@@ -6,11 +6,17 @@ export default class ControlPanelView {
   private sliderWrapper: HTMLDivElement;
   private thumbOneValue: TextField;
   private thumbTwoValue: TextField;
+  private min: TextField;
+  private max: TextField;
+  private step: TextField;
 
   constructor(sliderWrapper: HTMLDivElement) {
     this.sliderWrapper = sliderWrapper;
     this.thumbOneValue = new TextField('First thumb value');
     this.thumbTwoValue = new TextField('Second thumb value');
+    this.min = new TextField('min value');
+    this.max = new TextField('max value');
+    this.step = new TextField('step');
 
     this.createPanel();
   }
@@ -23,6 +29,15 @@ export default class ControlPanelView {
         'slider-panel__text-fields-group',
         this.thumbOneValue.getControl(),
         this.thumbTwoValue.getControl()
+      )
+    );
+
+    container.append(
+      groupElements(
+        'slider-panel__text-fields-group',
+        this.min.getControl(),
+        this.max.getControl(),
+        this.step.getControl()
       )
     );
 
