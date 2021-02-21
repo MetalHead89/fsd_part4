1570531c2b1ef7ae2736adfe254bc2fd6f9d55f6
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-02-21 23:03:30 +0700

    Added IGroupElements interface

--- src/plugins/control-panel/interfaces.ts
+++ src/plugins/control-panel/interfaces.ts
@@ -2,3 +2,9 @@ export interface IRadioParams {
   label: string;
   value: string;
 }
+
+export interface IGroupElements {
+  header?: string;
+  wrapperClass: string;
+  elements: HTMLDivElement[];
+}
