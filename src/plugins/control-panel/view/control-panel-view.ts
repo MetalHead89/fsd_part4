64bce43777331e5f9f5efe5b757400a124035b4c
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-02-21 23:07:30 +0700

    Added the ability to add a header to the wrapper in the group-elements module

--- src/plugins/control-panel/view/control-panel-view.ts
+++ src/plugins/control-panel/view/control-panel-view.ts
@@ -38,25 +38,41 @@ export default class ControlPanelView {
     const container = new Container();
 
     container.append(
-      groupElements(
-        'slider-panel__text-fields-group',
-        this.thumbOneValue.getControl(),
-        this.thumbTwoValue.getControl()
-      )
+      groupElements({
+        wrapperClass: 'control-panel__text-fields-group',
+        elements: [
+          this.thumbOneValue.getControl(),
+          this.thumbTwoValue.getControl(),
+        ],
+      })
     );
 
     container.append(
-      groupElements(
-        'slider-panel__text-fields-group',
-        this.min.getControl(),
-        this.max.getControl(),
-        this.step.getControl()
-      )
+      groupElements({
+        wrapperClass: 'control-panel__text-fields-group',
+        elements: [
+          this.min.getControl(),
+          this.max.getControl(),
+          this.step.getControl(),
+        ],
+      })
     );
 
-    container.append(groupElements('', this.typeRadio.getControl()));
+    container.append(
+      groupElements({
+        header: 'type',
+        wrapperClass: '',
+        elements: [this.typeRadio.getControl()],
+      })
+    );
 
-    container.append(groupElements('', this.orientationRadio.getControl()));
+    container.append(
+      groupElements({
+        header: 'orientation',
+        wrapperClass: '',
+        elements: [this.orientationRadio.getControl()],
+      })
+    );
 
     this.sliderWrapper.append(container.getElement());
   }
--- src/plugins/control-panel/view/group-elements.ts
+++ src/plugins/control-panel/view/group-elements.ts
@@ -1,22 +1,28 @@
+import { IGroupElements } from '../interfaces';
+
 /**
  * Оборачивает полученные элементы в div с заданным классом
  * @param {string} wrapperClass - класс, который будет назначен обёртке
  * @param {HTMLElement[]} - массив элементов, которые требуется обернуть
  * @returns {HTMLDivElement} - элементы в обёртке
  */
-export default function wrapElements(
-  wrapperClass: string,
-  ...elements: HTMLDivElement[]
-): HTMLDivElement {
+export default function groupElements(params: IGroupElements): HTMLDivElement {
   const wrapper: HTMLDivElement = document.createElement('div');
-  wrapper.className = wrapperClass;
+  wrapper.className = params.wrapperClass;
 
   for (
     let elementIndex = 0;
-    elementIndex < elements.length;
+    elementIndex < params.elements.length;
     elementIndex += 1
   ) {
-    wrapper.append(elements[elementIndex]);
+    if (params.header !== undefined) {
+      const header = document.createElement('label');
+      header.classList.add('control-panel__radio-group-header');
+      header.innerText = params.header;
+      wrapper.append(header);
+    }
+
+    wrapper.append(params.elements[elementIndex]);
   }
 
   return wrapper;
