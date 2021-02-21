14b2c52352d15a9ac2c3144fdf452b5ea3ee3422
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-02-21 14:35:25 +0700

    Added a group of elements for input / output of thumbs values

--- src/plugins/control-panel/view/control-panel-view.ts
+++ src/plugins/control-panel/view/control-panel-view.ts
@@ -1,20 +1,30 @@
 import Container from './container/container';
 import TextField from './text-field/text-field';
+import groupElements from './group-elements';
 
 export default class ControlPanelView {
   private sliderWrapper: HTMLDivElement;
   private thumbOneValue: TextField;
+  private thumbTwoValue: TextField;
 
   constructor(sliderWrapper: HTMLDivElement) {
     this.sliderWrapper = sliderWrapper;
     this.thumbOneValue = new TextField('First thumb value');
+    this.thumbTwoValue = new TextField('Second thumb value');
 
     this.createPanel();
   }
 
   private createPanel(): void {
     const container = new Container();
-    container.append(this.thumbOneValue.getControl());
+
+    container.append(
+      groupElements(
+        'slider-panel__text-fields-group',
+        this.thumbOneValue.getControl(),
+        this.thumbTwoValue.getControl()
+      )
+    );
 
     this.sliderWrapper.append(container.getElement());
   }
