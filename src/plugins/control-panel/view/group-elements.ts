3c6d9ade468b5aec4b7e061830ebf8c38ea96e50
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-02-22 13:05:42 +0700

    Fixed incorrect positioning of the header in the group-elements module

--- src/plugins/control-panel/view/group-elements.ts
+++ src/plugins/control-panel/view/group-elements.ts
@@ -10,18 +10,18 @@ export default function groupElements(params: IGroupElements): HTMLDivElement {
   const wrapper: HTMLDivElement = document.createElement('div');
   wrapper.className = params.wrapperClass;
 
+  if (params.header !== undefined) {
+    const header = document.createElement('label');
+    header.classList.add('control-panel__radio-group-header');
+    header.innerText = params.header;
+    wrapper.append(header);
+  }
+
   for (
     let elementIndex = 0;
     elementIndex < params.elements.length;
     elementIndex += 1
   ) {
-    if (params.header !== undefined) {
-      const header = document.createElement('label');
-      header.classList.add('control-panel__radio-group-header');
-      header.innerText = params.header;
-      wrapper.append(header);
-    }
-
     wrapper.append(params.elements[elementIndex]);
   }
 
