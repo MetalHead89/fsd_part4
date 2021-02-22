e2f20faba082d67a10af3be6692df1917b352813
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-02-22 20:59:30 +0700

    Added ControlPanelModel class

---
+++ src/plugins/control-panel/model/control-panel-model.ts
@@ -0,0 +1,13 @@
+import { IThumbsValues } from '../../simple-slider/interfaces';
+
+export default class ControlPanelModel {
+  private slider: JQuery<HTMLElement>;
+
+  constructor(slider: JQuery<HTMLElement>) {
+    this.slider = slider;
+  }
+
+  getThumbsValues(): IThumbsValues {
+    return this.slider.simpleSlider('getThumbsValues');
+  }
+}
