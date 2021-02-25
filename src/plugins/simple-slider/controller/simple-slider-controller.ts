84cfab900c2455673c183392f2b67404b3e4171a
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-02-25 19:27:34 +0700

    The code in SimpleSliderController is commented out for further refactoring

--- src/plugins/simple-slider/controller/simple-slider-controller.ts
+++ src/plugins/simple-slider/controller/simple-slider-controller.ts
@@ -19,51 +19,49 @@ class SimpleSliderController implements IObserver {
     this.simpleSliderView.register('thumbIsDragged', this);
     this.simpleSliderModel.register('scaleOn', this);
 
-    this.init(params.settings);
+    // this.init(params.settings);
   }
 
-  /**
-   * Инициализация слайдера в оответствии с полученными настройками
-   * @param {ISliderSettings} sliderSettings - объект с настройками слайдера
-   */
-  private init(sliderSettings: ISliderSettings): void {
-    const settings = { ...sliderSettings };
-    const sliderSize = this.simpleSliderView.getSliderSize();
-    const thumbSize = this.simpleSliderView.getThumbSize();
+  // /**
+  //  * Инициализация слайдера в оответствии с полученными настройками
+  //  * @param {ISliderSettings} sliderSettings - объект с настройками слайдера
+  //  */
+  // private init(sliderSettings: ISliderSettings): void {
+  // const settings = { ...sliderSettings };
+  // const sliderSize = this.simpleSliderView.getSliderSize();
+  // const thumbSize = this.simpleSliderView.getThumbSize();
 
-    settings.sliderSize = sliderSize;
-    settings.thumbSize = thumbSize;
-    this.simpleSliderModel.refreshSliderState(settings);
-  }
+  // settings.sliderSize = sliderSize;
+  // settings.thumbSize = thumbSize;
+  // this.simpleSliderModel.refreshSliderState(settings);
+  // }
 
-  /**
-   * Перехватывание и реагирование на уведомления от SimpleSliderModel и SimpleSliderView
-   * @param {string} eventType - тип уведомления
-   */
+  // /**
+  //  * Перехватывание и реагирование на уведомления от SimpleSliderModel и SimpleSliderView
+  //  * @param {string} eventType - тип уведомления
+  //  */
   update(eventType: string): void {
-    if (eventType === 'thumbIsDragged') {
-      this.simpleSliderModel.updateThumbsState(
-        this.simpleSliderView.getThumbsPos(),
-      );
-    }
-
-    if (eventType === 'thumbsPosIsUpdated') {
-      this.simpleSliderView.updateThumbs(this.simpleSliderModel.getThumbsPos());
-      this.simpleSliderView.updatePopUps(
-        this.simpleSliderModel.getPopUpsParams(),
-      );
-      this.simpleSliderView.updateProgressBar(
-        this.simpleSliderModel.getProgressBarParams(),
-      );
-    }
-
-    if (eventType === 'scaleOn') {
-      const max = this.simpleSliderModel.getMax();
-      const points = this.simpleSliderModel.getScalePoints(
-        this.simpleSliderView.getScalePointSize(max),
-      );
-      this.simpleSliderView.addScalePoints(points);
-    }
+    //   if (eventType === 'thumbIsDragged') {
+    //     this.simpleSliderModel.updateThumbsState(
+    //       this.simpleSliderView.getThumbsPos(),
+    //     );
+    //   }
+    //   if (eventType === 'thumbsPosIsUpdated') {
+    //     this.simpleSliderView.updateThumbs(this.simpleSliderModel.getThumbsPos());
+    //     this.simpleSliderView.updatePopUps(
+    //       this.simpleSliderModel.getPopUpsParams(),
+    //     );
+    //     this.simpleSliderView.updateProgressBar(
+    //       this.simpleSliderModel.getProgressBarParams(),
+    //     );
+    //   }
+    //   if (eventType === 'scaleOn') {
+    //     const max = this.simpleSliderModel.getMax();
+    //     const points = this.simpleSliderModel.getScalePoints(
+    //       this.simpleSliderView.getScalePointSize(max),
+    //     );
+    //     this.simpleSliderView.addScalePoints(points);
+    // }
   }
 }
 
