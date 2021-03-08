901630d97305d825e64be5e70c255cb97d68d1c5
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-03-08 16:52:04 +0700

    Удалено лишнее поле TEST_OK из мока класса Container

--- src/plugins/simple-slider/view/container/__mocks__/container.ts
+++ src/plugins/simple-slider/view/container/__mocks__/container.ts
@@ -3,8 +3,6 @@ import Element from '../../element/element';
 jest.mock('../../element/element');
 
 export default class Container extends Element {
-  testOk = true;
-
   constructor() {
     super('slider');
   }
