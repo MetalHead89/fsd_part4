feba848d0291397612a61f44a2efaaba07fe49e8
Bakalov Vitalij <bakalov.vitaliy@gmail.com>
2021-03-11 05:21:32 +0700

    Добавлены документирующие комментарии к методам класса Track

--- src/plugins/simple-slider/view/track/track.ts
+++ src/plugins/simple-slider/view/track/track.ts
@@ -1,19 +1,24 @@
 import Element from '../element/element';
 
-class Track extends Element {
+export default class Track extends Element {
   constructor() {
     super('slider__track');
     this.init();
   }
 
+  /**
+   * Инициализация трека, подключение обработчиков событий
+   */
   private init(): void {
     this.element.addEventListener('click', this.clickToTrack.bind(this));
   }
 
+  /**
+   * Обработка клика по треку
+   * @param {MouseEvent} event - объект события click
+   */
   private clickToTrack(event: MouseEvent): void {
     this.setPosition({ left: event.clientX, top: event.clientY });
     this.subject.notify('clickToTrack');
   }
 }
-
-export default Track;
