10f2946d23df7f0d0bffe7cd18b0c300a0ec77fb
Bakalov Vitalij <bakalov.vitaliy@gmail.com>
2021-03-11 05:02:05 +0700

    Добавлены документируюз=щие комментарии к методам класса PopUp

--- src/plugins/simple-slider/view/pop-up/pop-up.ts
+++ src/plugins/simple-slider/view/pop-up/pop-up.ts
@@ -1,24 +1,34 @@
 import { IPopUpParams, IPosition } from '../../interfaces';
 import Element from '../element/element';
 
-class PopUp extends Element {
+export default class PopUp extends Element {
   constructor(orientation?: string) {
     super('slider__pop-up', orientation);
   }
 
+  /**
+   * Обновляет позицию всплывающей подсказки и её значение
+   * @param {IPopUpParams} params - объект с новой позицией и значением всплывающей подсказки
+   */
   update(params: IPopUpParams): void {
     this.updateValue(params.value);
     this.updatePosition(params.position);
   }
 
+  /**
+   * Обновляет значение всплывающей подсказки
+   * @param {number} value - новое значение всплывающей подсказки
+   */
   private updateValue(value: number): void {
     this.element.innerHTML = `${value}`;
   }
 
+  /**
+   * Обновляет позицию всплывающей подсказки
+   * @param {IPosition} position - объект с новой позицией всплывающей подсказки
+   */
   private updatePosition(position: IPosition): void {
     this.element.style.left = `${position.left}px`;
     this.element.style.top = `${position.top}px`;
   }
 }
-
-export default PopUp;
