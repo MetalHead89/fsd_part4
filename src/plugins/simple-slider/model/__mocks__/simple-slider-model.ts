217f12c2eb7245031370e84090db1a66bf7b3d37
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-03-10 22:27:52 +0700

    Добавлен мок класса Model

---
+++ src/plugins/simple-slider/model/__mocks__/simple-slider-model.ts
@@ -0,0 +1,63 @@
+import { ISliderSettings } from '../../interfaces';
+
+export default class Model {
+  private settings: ISliderSettings;
+  private TEST_OK = true;
+  subject = {
+    register: (): boolean => true,
+    unsubscribe: (): boolean => true,
+    notify: (): boolean => true,
+  };
+
+  constructor(settings: ISliderSettings) {
+    this.settings = settings;
+  }
+
+  setSliderSize(): boolean {
+    return this.TEST_OK;
+  }
+
+  setThumbSize(): boolean {
+    return this.TEST_OK;
+  }
+
+  getOrientation(): string {
+    return this.settings.orientation;
+  }
+
+  getType(): string {
+    return this.settings.type;
+  }
+
+  getPopUpsState(): boolean {
+    return this.settings.popUps;
+  }
+
+  getThumbsPos(): boolean {
+    return this.TEST_OK;
+  }
+
+  updateThumbsState(): boolean {
+    return this.TEST_OK;
+  }
+
+  getScaleState(): boolean {
+    return this.settings.scale;
+  }
+
+  getMin(): number {
+    return this.settings.min;
+  }
+
+  getMax(): number {
+    return this.settings.max;
+  }
+
+  getStep(): number {
+    return this.settings.step;
+  }
+
+  getScalePoints(): boolean {
+    return this.TEST_OK;
+  }
+}
