703c191e8590150ffeb180bccf931f7dfdd4fe61
Bakalov Vitalij <bakalov.vitaliy@gmail.com>
2021-03-11 05:01:40 +0700

    Добавлены документируюз=щие комментарии к методам класса Container

--- src/plugins/simple-slider/view/container/container.ts
+++ src/plugins/simple-slider/view/container/container.ts
@@ -1,13 +1,15 @@
 import Element from '../element/element';
 
-class Container extends Element {
+export default class Container extends Element {
   constructor() {
     super('slider');
   }
 
+  /**
+   * Добавляет принятый div элемент в конец контейнера
+   * @param {HTMLDivElement} control - div элемент, который должен быть добавлен в контейнер
+   */
   append(control: HTMLDivElement): void {
     this.element.append(control);
   }
 }
-
-export default Container;
