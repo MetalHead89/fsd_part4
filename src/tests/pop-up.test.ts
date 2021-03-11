0c2584c6c4e1e0a7a88b77e73085c29db8fd8cd5
Bakalov Vitalij <bakalov.vitaliy@gmail.com>
2021-03-12 05:03:22 +0700

    Добавлены тесты для класса PopUp

---
+++ src/tests/pop-up.test.ts
@@ -0,0 +1,36 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import PopUp from '../plugins/simple-slider/view/pop-up/pop-up';
+
+let popUp: PopUp;
+
+beforeEach(() => {
+  popUp = new PopUp();
+});
+
+describe('Update', () => {
+  test('Popup value should be 50', () => {
+    const params = { value: 50, position: { left: 30, top: 50 } };
+    popUp.update(params);
+    expect(popUp.getElement().innerHTML).toBe('50');
+  });
+  test('Popup value should be 120', () => {
+    const params = { value: 120, position: { left: 30, top: 50 } };
+    popUp.update(params);
+    expect(popUp.getElement().innerHTML).toBe('120');
+  });
+  test('Popup position should be { left: 30, top: 50 }', () => {
+    const params = { value: 50, position: { left: 30, top: 50 } };
+    popUp.update(params);
+    expect(popUp.getElement().style.left).toBe('30px');
+    expect(popUp.getElement().style.top).toBe('50px');
+  });
+  test('Popup position should be { left: 90, top: 14 }', () => {
+    const params = { value: 50, position: { left: 90, top: 14 } };
+    popUp.update(params);
+    expect(popUp.getElement().style.left).toBe('90px');
+    expect(popUp.getElement().style.top).toBe('14px');
+  });
+});
