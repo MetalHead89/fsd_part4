f661d7b5f826781343eafbb8459f3e8200ff54e2
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-03-12 21:42:33 +0700

    Добавлены документирующие комментрии к методам класса Container

--- src/plugins/control-panel/view/container/container.ts
+++ src/plugins/control-panel/view/container/container.ts
@@ -1,14 +1,23 @@
 export default class Container {
   private element: HTMLDivElement;
+
   constructor() {
     this.element = document.createElement('div');
     this.element.classList.add('control-panel');
   }
 
+  /**
+   * Возвращает HTML элемент контейнера
+   * @returns {HTMLDivElement} - div элемент
+   */
   getElement(): HTMLDivElement {
     return this.element;
   }
 
+  /**
+   * Добавляет принятый div элемент в конец контейнера
+   * @param {HTMLDivElement} control - div элемент, который должен быть добавлен в контейнер
+   */
   append(control: HTMLDivElement): void {
     this.element.append(control);
   }
