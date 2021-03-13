156454f4e3f7d5eb0b22e05be0284f1f1c5e3efc
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-03-13 18:45:43 +0700

    Добавлены тесты метода setThumbShift из класса Thumb

--- src/tests/thumb.test.ts
+++ src/tests/thumb.test.ts
@@ -2,6 +2,8 @@
  * @jest-environment jsdom
  */
 
+/* eslint-disable dot-notation */
+
 import Thumb from '../plugins/simple-slider/view/thumb/thumb';
 
 let thumb: Thumb;
@@ -49,5 +51,18 @@ describe('Thumb dragstart event should return false', () => {
   test('Event should return false', () => {
     const evt = new window.Event('dragstart');
     expect(thumb.getElement().dispatchEvent(evt)).toBe(false);
+  });
+});
+
+describe('Set thumb shift', () => {
+  test('Thumb shift should be {shiftX: 2, shiftY: 5}', () => {
+    thumb['setThumbShift']({ left: 85, top: 47 }, { left: 83, top: 42 });
+    expect(thumb['shift'].shiftX).toBe(2);
+    expect(thumb['shift'].shiftY).toBe(5);
+  });
+  test('Thumb shift should be {shiftX: 5, shiftY: 3}', () => {
+    thumb['setThumbShift']({ left: 100, top: 80 }, { left: 95, top: 77 });
+    expect(thumb['shift'].shiftX).toBe(5);
+    expect(thumb['shift'].shiftY).toBe(3);
   });
 });
