8a490824c0c68807e425158e5a164909598e6ac2
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-03-13 11:38:49 +0700

    Добавлены тесты для метода setZIndex из класса Thumb

---
+++ src/tests/thumb.test.ts
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import Thumb from '../plugins/simple-slider/view/thumb/thumb';
+
+let thumb: Thumb;
+
+beforeEach(() => {
+  thumb = new Thumb();
+});
+
+describe('Set z-index', () => {
+  test('Thumb z-inex should be 5', () => {
+    thumb.setZIndex(5);
+    expect(thumb.getElement().style.zIndex).toBe('5');
+  });
+  test('Thumb z-inex should be 8', () => {
+    thumb.setZIndex(8);
+    expect(thumb.getElement().style.zIndex).toBe('8');
+  });
+});
