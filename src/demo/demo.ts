b127a16352449e9b342d060ab378d7ed30f1ffbe
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-03-15 08:42:40 +0700

    Добавлена логика создания и удаления слайдеров в demo.ts

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,4 +1,47 @@
-const sliderWrapper = document.querySelectorAll('.slider-wrapper');
-if (sliderWrapper !== null) {
-  $(sliderWrapper).simpleSlider().controlPanel();
+// const sliderWrapper = document.querySelectorAll('.slider-wrapper');
+// if (sliderWrapper !== null) {
+//   $(sliderWrapper).simpleSlider().controlPanel();
+// }
+
+const addNewSliderBtn = document.querySelector('.add-new-slider-btn');
+let helpImage = document.querySelector('.help-image');
+const buttonWrapper = document.querySelector('.button-wrapper');
+
+function removeSlider(sliderWrapper: HTMLDivElement) {
+  sliderWrapper.remove();
+  if (document.querySelectorAll('.slider-wrapper').length === 0) {
+    if (addNewSliderBtn !== null) {
+      addNewSliderBtn.classList.add('add-new-slider-btn_margin-top');
+    }
+  }
 }
+
+function clickToAddNewSliderBtn(): void {
+  if (buttonWrapper !== null) {
+    helpImage?.remove();
+    helpImage = null;
+
+    addNewSliderBtn?.classList.remove('add-new-slider-btn_margin-top');
+
+    const sliderWrapper = document.createElement('div');
+    sliderWrapper.classList.add('slider-wrapper');
+    buttonWrapper.before(sliderWrapper);
+
+    const icon = document.createElement('span');
+    icon.classList.add('material-icons', 'remove-slider-button__icon');
+    icon.innerText = 'clear';
+
+    const removeSliderButton = document.createElement('button');
+    removeSliderButton.classList.add('remove-slider-button');
+    removeSliderButton.append(icon);
+    removeSliderButton.addEventListener(
+      'click',
+      removeSlider.bind(removeSliderButton, sliderWrapper),
+    );
+    sliderWrapper.append(removeSliderButton);
+
+    $(sliderWrapper).simpleSlider().controlPanel();
+  }
+}
+
+addNewSliderBtn?.addEventListener('click', clickToAddNewSliderBtn);
