097e469fd14c88d86766e0e8dea74cb4aa218ee7
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-03-16 10:35:08 +0700

    В мок класса контейнер добавлены методы resetMargns и setMargins

--- src/plugins/simple-slider/view/container/__mocks__/container.ts
+++ src/plugins/simple-slider/view/container/__mocks__/container.ts
@@ -3,11 +3,21 @@ import Element from '../../element/element';
 jest.mock('../../element/element');
 
 export default class Container extends Element {
+  TEST_OK = true;
+
   constructor() {
     super('slider');
   }
 
   append(element: HTMLDivElement): void {
     this.element.append(element);
+  }
+
+  resetMargins(): boolean {
+    return this.TEST_OK;
+  }
+
+  setMargins(): boolean {
+    return this.TEST_OK;
   }
 }
