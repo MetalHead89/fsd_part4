301233464038149780b52ffb0026966be66b5c12
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-03-16 10:03:44 +0700

    Добавлены тесты для метода resetMargins из класса Container

--- src/tests/container.test.ts
+++ src/tests/container.test.ts
@@ -27,7 +27,7 @@ describe('Append', () => {
 });
 
 describe('Set margins', () => {
-  test('Container margins should be {left: 25, top: 77, right: 13, bottom: 2', () => {
+  test('Container margins should be {left: 25, top: 77, right: 13, bottom: 2}', () => {
     container.setMargins({
       left: 25,
       top: 77,
@@ -40,7 +40,7 @@ describe('Set margins', () => {
     expect(container['element'].style.marginRight).toBe('13px');
     expect(container['element'].style.marginBottom).toBe('2px');
   });
-  test('Container margins should be {left: 84, top: 97, right: 67, bottom: 73', () => {
+  test('Container margins should be {left: 84, top: 97, right: 67, bottom: 73}', () => {
     container.setMargins({
       left: 84,
       top: 97,
@@ -52,5 +52,36 @@ describe('Set margins', () => {
     expect(container['element'].style.marginTop).toBe('97px');
     expect(container['element'].style.marginRight).toBe('67px');
     expect(container['element'].style.marginBottom).toBe('73px');
+  });
+});
+
+describe('Reset margins', () => {
+  test('Container margins should be {left: 0, top: 0, right: 0, bottom: 0}', () => {
+    container.setMargins({
+      left: 25,
+      top: 77,
+      right: 13,
+      bottom: 2,
+    });
+
+    container.resetMargins();
+
+    expect(container['element'].style.marginLeft).toBe('0');
+    expect(container['element'].style.marginTop).toBe('0');
+    expect(container['element'].style.marginRight).toBe('0');
+    expect(container['element'].style.marginBottom).toBe('0');
+  });
+  test('Container margins should be {left: 0, top: 0, right: 0, bottom: 0}', () => {
+    container.setMargins({
+      left: 84,
+      top: 97,
+      right: 67,
+      bottom: 73,
+    });
+
+    expect(container['element'].style.marginLeft).toBe('0');
+    expect(container['element'].style.marginTop).toBe('0');
+    expect(container['element'].style.marginRight).toBe('0');
+    expect(container['element'].style.marginBottom).toBe('0');
   });
 });
