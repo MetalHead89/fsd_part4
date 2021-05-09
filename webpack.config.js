c866bc8a21b2eb88fc27d99928fcf8dd7ac6132a
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-05-09 23:25:11 +0700

    Добавлен код формирующий название сторонних модулей в сборке

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,18 @@ module.exports = {
   optimization: {
     splitChunks: {
       chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
     },
   },
   devServer: {
