eaeeabc27fc6bd64b4a7384cb6e96b0069d2d959
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-09-05 21:10:06 +0700

    Исправлен импорт скриптов в index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@ require.context('', true, /\.scss$/);
 
 const scripts = require.context('', true, /[^d]\.ts$/);
 scripts.keys().forEach((script) => {
-  scripts(script);
+  const directories = script.split('/');
+
+  if (!directories.includes('__mocks__') && !directories.includes('tests')) {
+    scripts(script);
+  }
 });
