9992eec4cbfe31e82ce8b079be04f0662f2ca0ac
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-10-11 23:17:40 +0700

    Удалена ненужная конфигурация webpack.simple-js-slider.js

--- webpack.simple-js-slider.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const path = require('path');
-
-module.exports = {
-  entry: path.resolve(__dirname, 'src/plugins/simple-js-slider/index.ts'),
-};
