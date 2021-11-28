b8922717a4a2059bdc400751f68da6f46345df9e
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-11-28 19:45:35 +0700

    Удален лишний код из SimpleJsSliderController

--- src/plugins/simple-js-slider/controller/simple-js-slider-controller.ts
+++ src/plugins/simple-js-slider/controller/simple-js-slider-controller.ts
@@ -1,68 +1,24 @@
-/* eslint-disable comma-dangle */
-
-import {
-  IControllerParams,
-  // IObserver,
-  // IPosition,
-  ISimpleJsSliderModel,
-  ISimpleJsSliderView,
-  // ISubject,
-  // ISubjectEvents,
-  // IThumbsPositions,
-} from '../interfaces';
+import { IControllerParams, ISimpleJsSliderModel, ISimpleJsSliderView } from '../interfaces';
 import { IThumbsPositionsNew } from '../new-interfaces';
 
 class SimpleJsSliderController {
   private model: ISimpleJsSliderModel;
   private view: ISimpleJsSliderView;
-  // private modelSubject: ISubject;
-  // private viewSubject: ISubject;
-  // private events: ISubjectEvents = {
-  //   thumbIsDragged: () => {
-  //     this.model.updateThumbsState(this.view.getThumbsPositions());
-  //   },
-  //   thumbsPositionsIsUpdated: () => this.updateThumbsPositions(),
-  //   minIsUpdated: () => {
-  //     this.updateView();
-  //     this.model.recalculateStep();
-  //   },
-  //   maxIsUpdated: () => {
-  //     this.updateView();
-  //     this.model.recalculateStep();
-  //   },
-  //   stepIsUpdated: () => this.updateView(),
-  //   orientationIsUpdated: () => this.updateSliderOrientation(),
-  //   typeIsUpdated: () => this.updateSliderType(),
-  //   scaleStateIsUpdated: () => this.updateScaleState(),
-  //   popUpsStateIsUpdated: () => this.updatePopUpsState(),
-  //   clickToTrack: () => {
-  //     this.model.setThumbPositionOnClickPosition(
-  //       this.view.getTrackClickPosition()
-  //     );
-  //   },
-  //   clickToScale: () => {
-  //     this.model.setThumbPositionOnClickPosition(
-  //       this.view.getScaleClickPosition()
-  //     );
-  //   },
-  //   thumbsSwapped: () => this.view.swapThumbs(),
-  //   windowResized: () => this.init(),
-  // };
 
   constructor(params: IControllerParams) {
     this.model = params.model;
     this.view = params.view;
-    // this.modelSubject = this.model.subject;
-    // this.viewSubject = this.view.subject;
 
     this.init();
-    // this.subscribeToEvents();
     this.bindContext();
-    this.subscribeToEventsNew();
+    this.subscribeToEvents();
   }
 
-  //NEW_METHODS//
-  private subscribeToEventsNew(): void {
+  private init(): void {
+    this.fullViewUpdate();
+  }
+
+  private subscribeToEvents(): void {
     this.view.observer.register('thumbIsDragged', (args: IThumbsPositionsNew) => {
       this.model.updateThumbsValues(args);
     });
@@ -78,29 +34,11 @@ class SimpleJsSliderController {
 
   private updateView(): void {
     this.view.moveThumbs(this.model.getThumbsPositionsNew());
-    // this.view.updatePopUps(this.model.getThumbValues());
     this.view.updateProgressBar();
   }
 
   private updateScale() {
     this.view.updateScale(this.model.getPointsParams());
-  }
-  //END_NEW_METHODS//
-
-  // update(eventType: string): void {
-  //   if (eventType in this.events) {
-  //     this.events[eventType]();
-  //   }
-  // }
-
-  private init(): void {
-    this.fullViewUpdate();
-    // this.model.setSliderSize(this.view.getSliderSize());
-    // this.model.setThumbSize(this.view.getThumbSize());
-    // this.updateSliderType();
-    // this.updatePopUpsState();
-
-    // this.updateView();
   }
 
   private fullViewUpdate(): void {
@@ -108,48 +46,9 @@ class SimpleJsSliderController {
     this.updateSliderType();
     this.updatePopUpsState();
     this.updateView();
-    // this.updatePopUpsState();
     this.updateScaleState();
     this.updateScale();
   }
-
-  // private subscribeToEvents(): void {
-  //   this.viewSubject.register('thumbIsDragged', this);
-  //   this.modelSubject.register('thumbsPositionsIsUpdated', this);
-  //   this.modelSubject.register('minIsUpdated', this);
-  //   this.modelSubject.register('maxIsUpdated', this);
-  //   this.modelSubject.register('stepIsUpdated', this);
-  //   this.modelSubject.register('typeIsUpdated', this);
-  //   this.modelSubject.register('orientationIsUpdated', this);
-  //   this.modelSubject.register('scaleStateIsUpdated', this);
-  //   this.modelSubject.register('popUpsStateIsUpdated', this);
-  //   this.modelSubject.register('thumbsSwapped', this);
-  //   this.view.subject.register('clickToTrack', this);
-  //   this.view.subject.register('clickToScale', this);
-  //   this.view.subject.register('windowResized', this);
-  // }
-
-  // private updateView(): void {
-  //   this.model.updateThumbsState(this.model.getThumbsPositions());
-
-  //   if (this.model.isScaleEnabled()) {
-  //     this.view.enableScale();
-
-  //     const max = this.model.getMax();
-  //     this.model.setScalePointSize(this.view.getScalePointSize(max));
-
-  //     const points = this.model.getScalePoints();
-  //     this.view.addScalePoints(points);
-  //   } else {
-  //     this.view.disableScale();
-  //   }
-  // }
-
-  // private updateThumbsPositions(): void {
-  //   this.view.updateThumbs(this.model.getThumbsPositions());
-  //   this.view.updatePopUps(this.model.getPopUpsParams());
-  //   this.view.updateProgressBar(this.model.getProgressBarParams());
-  // }
 
   private updateSliderOrientation(): void {
     if (this.model.getOrientation() === 'horizontal') {
@@ -159,7 +58,6 @@ class SimpleJsSliderController {
     }
 
     this.model.setSliderSize(this.view.getSliderSize());
-    // this.updateView();
   }
 
   private updateSliderType(): void {
@@ -168,8 +66,6 @@ class SimpleJsSliderController {
     } else {
       this.view.switchToRange();
     }
-
-    // this.model.updateThumbsState(this.model.getThumbsPositions());
   }
 
   private updateScaleState() {
@@ -184,7 +80,6 @@ class SimpleJsSliderController {
   private updatePopUpsState(): void {
     if (this.model.isPopUpsEnabled()) {
       this.view.enablePopUps();
-      // this.model.updateThumbsState(this.model.getThumbsPositions());
     } else {
       this.view.disablePopUps();
     }
@@ -192,173 +87,3 @@ class SimpleJsSliderController {
 }
 
 export default SimpleJsSliderController;
-
-// /* eslint-disable comma-dangle */
-
-// import {
-//   IControllerParams,
-//   IObserver,
-//   IPosition,
-//   ISimpleJsSliderModel,
-//   ISimpleJsSliderView,
-//   ISubject,
-//   ISubjectEvents,
-//   IThumbsPositions,
-// } from '../interfaces';
-// import { IThumbsPositionsNew } from '../new-interfaces';
-
-// class SimpleJsSliderController implements IObserver {
-//   private model: ISimpleJsSliderModel;
-//   private view: ISimpleJsSliderView;
-//   private modelSubject: ISubject;
-//   private viewSubject: ISubject;
-//   private events: ISubjectEvents = {
-//     thumbIsDragged: () => {
-//       this.model.updateThumbsState(this.view.getThumbsPositions());
-//     },
-//     thumbsPositionsIsUpdated: () => this.updateThumbsPositions(),
-//     minIsUpdated: () => {
-//       this.updateView();
-//       this.model.recalculateStep();
-//     },
-//     maxIsUpdated: () => {
-//       this.updateView();
-//       this.model.recalculateStep();
-//     },
-//     stepIsUpdated: () => this.updateView(),
-//     orientationIsUpdated: () => this.updateSliderOrientation(),
-//     typeIsUpdated: () => this.updateSliderType(),
-//     scaleStateIsUpdated: () => this.updateScaleState(),
-//     popUpsStateIsUpdated: () => this.updatePopUpsState(),
-//     clickToTrack: () => {
-//       this.model.setThumbPositionOnClickPosition(
-//         this.view.getTrackClickPosition()
-//       );
-//     },
-//     clickToScale: () => {
-//       this.model.setThumbPositionOnClickPosition(
-//         this.view.getScaleClickPosition()
-//       );
-//     },
-//     thumbsSwapped: () => this.view.swapThumbs(),
-//     windowResized: () => this.init(),
-//   };
-
-//   constructor(params: IControllerParams) {
-//     this.model = params.model;
-//     this.view = params.view;
-//     this.modelSubject = this.model.subject;
-//     this.viewSubject = this.view.subject;
-
-//     this.subscribeToEvents();
-//     this.init();
-//     this.subscribeToEventsNew();
-//   }
-
-//   //NEW_METHODS//
-//   private subscribeToEventsNew(): void {
-//     this.view.observer.register('thumbIsDragged', (args: IThumbsPositionsNew) => {
-//       this.model.updateThumbsValues(args);
-//     });
-
-//     this.model.observer.register('thumbIsUpdated', (args: IThumbsPositionsNew) => {
-//       this.view.moveThumbs(args);
-//     });
-//   }
-//   //END_NEW_METHODS//
-
-//   update(eventType: string): void {
-//     if (eventType in this.events) {
-//       this.events[eventType]();
-//     }
-//   }
-
-//   private init(): void {
-//     this.updateSliderOrientation();
-//     this.model.setSliderSize(this.view.getSliderSize());
-//     this.model.setThumbSize(this.view.getThumbSize());
-//     this.updateSliderType();
-//     this.updatePopUpsState();
-
-//     this.updateView();
-//   }
-
-//   private subscribeToEvents(): void {
-//     this.viewSubject.register('thumbIsDragged', this);
-//     this.modelSubject.register('thumbsPositionsIsUpdated', this);
-//     this.modelSubject.register('minIsUpdated', this);
-//     this.modelSubject.register('maxIsUpdated', this);
-//     this.modelSubject.register('stepIsUpdated', this);
-//     this.modelSubject.register('typeIsUpdated', this);
-//     this.modelSubject.register('orientationIsUpdated', this);
-//     this.modelSubject.register('scaleStateIsUpdated', this);
-//     this.modelSubject.register('popUpsStateIsUpdated', this);
-//     this.modelSubject.register('thumbsSwapped', this);
-//     this.view.subject.register('clickToTrack', this);
-//     this.view.subject.register('clickToScale', this);
-//     this.view.subject.register('windowResized', this);
-//   }
-
-//   private updateView(): void {
-//     this.model.updateThumbsState(this.model.getThumbsPositions());
-
-//     if (this.model.isScaleEnabled()) {
-//       this.view.enableScale();
-
-//       const max = this.model.getMax();
-//       this.model.setScalePointSize(this.view.getScalePointSize(max));
-
-//       const points = this.model.getScalePoints();
-//       this.view.addScalePoints(points);
-//     } else {
-//       this.view.disableScale();
-//     }
-//   }
-
-//   private updateThumbsPositions(): void {
-//     this.view.updateThumbs(this.model.getThumbsPositions());
-//     this.view.updatePopUps(this.model.getPopUpsParams());
-//     this.view.updateProgressBar(this.model.getProgressBarParams());
-//   }
-
-//   private updateSliderOrientation(): void {
-//     if (this.model.getOrientation() === 'horizontal') {
-//       this.view.switchToHorizontal();
-//     } else {
-//       this.view.switchToVertical();
-//     }
-
-//     this.model.setSliderSize(this.view.getSliderSize());
-//     this.updateView();
-//   }
-
-//   private updateSliderType(): void {
-//     if (this.model.getType() === 'single') {
-//       this.view.switchToSingle();
-//     } else {
-//       this.view.switchToRange();
-//     }
-
-//     this.model.updateThumbsState(this.model.getThumbsPositions());
-//   }
-
-//   private updateScaleState() {
-//     if (this.model.isScaleEnabled()) {
-//       this.view.enableScale();
-//       this.updateView();
-//     } else {
-//       this.view.disableScale();
-//     }
-//   }
-
-//   private updatePopUpsState(): void {
-//     if (this.model.isPopUpsEnabled()) {
-//       this.view.enablePopUps();
-//       this.model.updateThumbsState(this.model.getThumbsPositions());
-//     } else {
-//       this.view.disablePopUps();
-//     }
-//   }
-// }
-
-// export default SimpleJsSliderController;
