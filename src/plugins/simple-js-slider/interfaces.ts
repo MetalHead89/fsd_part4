49830ce4a228fb445c423f4513cbcaf5fe994d89
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-11-28 20:12:21 +0700

    Удален лишний код из interfaces

--- src/plugins/simple-js-slider/interfaces.ts
+++ src/plugins/simple-js-slider/interfaces.ts
@@ -19,14 +19,6 @@ interface ISliderSettings {
   thumbSize?: ISize;
 }
 
-interface IObserver {
-  update(eventType: string): void;
-}
-
-interface IObserversList {
-  [index: string]: IObserver[];
-}
-
 interface ISubjectEvents {
   [key: string]: () => void;
 }
@@ -34,12 +26,6 @@ interface ISubjectEvents {
 interface IThumbsValues {
   thumbOne: number;
   thumbTwo: number;
-}
-
-interface ISubject {
-  register(eventType: string, observer: IObserver): void;
-  unsubscribe(eventType: string, observer: IObserver): void;
-  notify(eventType: string): void;
 }
 
 interface IPosition {
@@ -108,24 +94,10 @@ interface ISimpleJsSliderModel {
   getThumbValues(): IThumbsValues;
   getPointsParams(): IPointParams[];
   updateSliderSettings(settings: ISliderSettings): void;
-
-  // subject: ISubject;
-  // setSliderSize(size: ISize): void;
-  // setThumbSize(size: ISize): void;
-  // getMax(): number;
   getType(): string;
   isScaleEnabled(): boolean;
   isPopUpsEnabled(): boolean;
-  // refreshSliderState(settings: ISliderSettings): void;
-  // getThumbsPositions(): IThumbsPositions;
-  // updateThumbsState(positions: IThumbsPositions): void;
-  // getProgressBarParams(): IProgressBarParams;
-  // getPopUpsParams(): IPopUps;
-  // getScalePoints(): IScalePointParams[];
-  // setScalePointSize(size: ISize): void;
   getOrientation(): string;
-  // setThumbPositionOnClickPosition(clickPosition: IPosition): void;
-  // recalculateStep(): void;
 }
 
 interface ISimpleJsSliderView {
@@ -134,7 +106,6 @@ interface ISimpleJsSliderView {
   updatePopUps(values: IThumbsValues): void;
   updateProgressBar(): void;
   updateScale(pointsParams: IPointParams[]): void;
-  
   switchToHorizontal(): void;
   switchToVertical(): void;
   switchToSingle(): void;
@@ -142,16 +113,11 @@ interface ISimpleJsSliderView {
   disablePopUps(): void;
   enablePopUps(): void;
   enableScale(): void;
-  // swapThumbs(): void;
   disableScale(): void;
   getThumbSize(): ISize;
   getSliderSize(): ISize;
   getThumbsPositions(): IThumbsPositions;
   updateThumbs(thumbsPositions: IThumbsPositions): void;
-  // updateProgressBar(params: IProgressBarParams): void;
-  // updatePopUps(params: IPopUps): void;
-  // getScalePointSize(value: number): ISize;
-  // addScalePoints(points: IScalePointParams[]): void;
   getTrackClickPosition(): IPosition;
   getScaleClickPosition(): IPosition;
 }
@@ -171,11 +137,8 @@ interface ISliderMargins {
 export {
   ISize,
   ISliderSettings,
-  IObserver,
-  IObserversList,
   ISubjectEvents,
   IThumbsValues,
-  ISubject,
   IPosition,
   IThumbsPositions,
   IProgressBarParams,
