029e1c9e63166480d5861f262af7008c2f9b7276
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-11-28 20:30:25 +0700

    Удалены лишние методы из simple-js-slider

--- src/plugins/simple-js-slider/simple-js-slider.ts
+++ src/plugins/simple-js-slider/simple-js-slider.ts
@@ -51,37 +51,7 @@ import SimpleJsSliderView from './view/simple-js-slider-view';
     },
     getObserver(): IObserver {
       return $(this).data().model.getObserver();
-    },
-    // getModelSubject(): ISubject {
-    //   return $(this).data().model.subject;
-    // },
-    // getThumbsValues(): IThumbsValues {
-    //   return $(this).data().model.getThumbsValues();
-    // },
-    // getMin(): number {
-    //   return $(this).data().model.getMin();
-    // },
-    // getMax(): number {
-    //   return $(this).data().model.getMax();
-    // },
-    // getStep(): number {
-    //   return $(this).data().model.getStep();
-    // },
-    // isScaleEnabled(): boolean {
-    //   return $(this).data().model.isScaleEnabled();
-    // },
-    // isPopUpsEnabled(): boolean {
-    //   return $(this).data().model.isPopUpsEnabled();
-    // },
-    // getType(): string {
-    //   return $(this).data().model.getType();
-    // },
-    // getOrientation(): string {
-    //   return $(this).data().model.getOrientation();
-    // },
-    // refreshSliderState(sliderSettings: ISliderSettings): void {
-    //   $(this).data().model.refreshSliderState(sliderSettings);
-    // },
+    }
   };
 
   $.fn.simpleJsSlider = function plug(action?: string | ISliderSettings, args?): any {
