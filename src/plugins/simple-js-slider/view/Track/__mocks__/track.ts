f637b7de88ef87e32400ddfceb0858273f1fbe43
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-12-01 08:03:57 +0700

    Удалены __mocks__

--- src/plugins/simple-js-slider/view/Track/__mocks__/track.ts
+++
@@ -1,9 +0,0 @@
-import UIControl from '../../UIControl/UIControl';
-
-jest.mock('../../ui-control/ui-control');
-
-export default class Track extends UIControl {
-  constructor() {
-    super('slider__track');
-  }
-}
